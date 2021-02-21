f4c8ef11305c8f9eac5f94e637f13ad7126dc72f
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-02-21 12:21:19 +0100

    Drop-in replacement for Tendermint Node

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/lazyledger/optimint
 
 go 1.15
 
-require github.com/lazyledger/lazyledger-core v0.0.0-20210205192123-585566317e51
+require github.com/lazyledger/lazyledger-core v0.0.0-20210221110551-8a8b8b5483ba
---
+++ node/node.go
@@ -0,0 +1,98 @@
+package node
+
+import (
+	"fmt"
+	"github.com/lazyledger/lazyledger-core/libs/log"
+	"github.com/lazyledger/lazyledger-core/libs/service"
+	tmnode "github.com/lazyledger/lazyledger-core/node"
+	"github.com/lazyledger/lazyledger-core/proxy"
+	"github.com/lazyledger/lazyledger-core/types"
+)
+
+var _ tmnode.NodeInterface = &Node{}
+
+type Node struct {
+	service.BaseService
+	eventBus *types.EventBus
+	proxyApp proxy.AppConns
+}
+
+func NewNode(
+	clientCreator proxy.ClientCreator,
+	logger log.Logger,
+) (*Node, error) {
+	proxyApp := proxy.NewAppConns(clientCreator)
+	proxyApp.SetLogger(logger.With("module", "proxy"))
+	if err := proxyApp.Start(); err != nil {
+		return nil, fmt.Errorf("error starting proxy app connections: %w", err)
+	}
+
+	eventBus := types.NewEventBus()
+	eventBus.SetLogger(logger.With("module", "events"))
+	if err := eventBus.Start(); err != nil {
+		return nil, err
+	}
+
+	node := &Node{
+		proxyApp: proxyApp,
+		eventBus: eventBus,
+	}
+	node.BaseService = *service.NewBaseService(logger, "Node", node)
+
+	return node, nil
+}
+
+
+func (n *Node) OnStart() error {
+	panic("implement me")
+}
+
+//func (n *Node) Start() error {
+//	panic("implement me")
+//}
+
+//func (n *Node) Stop() error {
+//	panic("implement me")
+//}
+
+//func (n *Node) Reset() error {
+//	panic("implement me")
+//}
+
+//func (n *Node) IsRunning() bool {
+//	panic("implement me")
+//}
+
+//func (n *Node) Quit() <-chan struct{} {
+//	panic("implement me")
+//}
+
+//func (n *Node) String() string {
+//	panic("implement me")
+//}
+
+func (n *Node) OnStop() {
+	panic("implement me")
+}
+
+
+func (n *Node) OnReset() error {
+	panic("implement me")
+}
+
+
+func (n *Node) SetLogger(logger log.Logger) {
+	n.Logger = logger
+}
+
+func (n *Node) ConfigureRPC() error {
+	panic("implement me")
+}
+
+func (n *Node) GetLogger() log.Logger {
+	return n.Logger
+}
+
+func (n *Node) EventBus() *types.EventBus {
+	panic("implement me")
+}
