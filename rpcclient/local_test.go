acb83deaf418930b698fcf9db4ba7b506833d0f7
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-03-30 13:50:27 +0200

    More BroadcastTx tests

--- rpcclient/local_test.go
+++ rpcclient/local_test.go
@@ -3,6 +3,7 @@ package rpcclient
 import (
 	"context"
 	"crypto/rand"
+	"github.com/lazyledger/lazyledger-core/libs/bytes"
 	"testing"
 
 	abci "github.com/lazyledger/lazyledger-core/abci/types"
@@ -39,15 +40,64 @@ func TestInfo(t *testing.T) {
 func TestCheckTx(t *testing.T) {
 	assert := assert.New(t)
 
-	mockApp, rpc := getRPC(t)
-	mockApp.On("CheckTx", mock.Anything).Return(abci.ResponseCheckTx{})
-
 	expectedTx := []byte("tx data")
+
+	mockApp, rpc := getRPC(t)
+	mockApp.On("CheckTx", abci.RequestCheckTx{Tx: expectedTx}).Once().Return(abci.ResponseCheckTx{})
 
 	res, err := rpc.CheckTx(context.Background(), expectedTx)
 	assert.NoError(err)
 	assert.NotNil(res)
-	mockApp.AssertCalled(t, "CheckTx", abci.RequestCheckTx{Tx: expectedTx, Type: abci.CheckTxType_New})
+	mockApp.AssertExpectations(t)
+}
+
+func TestBroadcastTxAsync(t *testing.T) {
+	assert := assert.New(t)
+
+	expectedTx := []byte("tx data")
+
+	mockApp, rpc := getRPC(t)
+	mockApp.On("CheckTx", abci.RequestCheckTx{Tx: expectedTx}).Return(abci.ResponseCheckTx{})
+
+	res, err := rpc.BroadcastTxAsync(context.Background(), expectedTx)
+	assert.NoError(err)
+	assert.NotNil(res)
+	assert.Empty(res.Code)
+	assert.Empty(res.Data)
+	assert.Empty(res.Log)
+	assert.Empty(res.Codespace)
+	assert.NotEmpty(res.Hash)
+	mockApp.AssertExpectations(t)
+}
+
+func TestBroadcastTxSync(t *testing.T) {
+	assert := assert.New(t)
+
+	expectedTx := []byte("tx data")
+	expectedResponse := abci.ResponseCheckTx{
+		Code:      1,
+		Data:      []byte("data"),
+		Log:       "log",
+		Info:      "info",
+		GasWanted: 0,
+		GasUsed:   0,
+		Events:    nil,
+		Codespace: "space",
+	}
+
+	mockApp, rpc := getRPC(t)
+
+	mockApp.On("CheckTx", abci.RequestCheckTx{Tx: expectedTx}).Return(expectedResponse)
+
+	res, err := rpc.BroadcastTxSync(context.Background(), expectedTx)
+	assert.NoError(err)
+	assert.NotNil(res)
+	assert.Equal(expectedResponse.Code, res.Code)
+	assert.Equal(bytes.HexBytes(expectedResponse.Data), res.Data)
+	assert.Equal(expectedResponse.Log, res.Log)
+	assert.Equal(expectedResponse.Codespace, res.Codespace)
+	assert.NotEmpty(res.Hash)
+	mockApp.AssertExpectations(t)
 }
 
 func getRPC(t *testing.T) (*mocks.Application, *Local) {
