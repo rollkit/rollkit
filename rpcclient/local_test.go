a2b6a51f8acc854b07a3c95b160087378c97571e
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-03-30 12:26:34 +0200

    Use proper mocks in RPC client testing

--- rpcclient/local_test.go
+++ rpcclient/local_test.go
@@ -9,11 +9,13 @@ import (
 	"github.com/lazyledger/lazyledger-core/libs/log"
 	"github.com/lazyledger/lazyledger-core/proxy"
 	"github.com/lazyledger/lazyledger-core/types"
-	crypto "github.com/libp2p/go-libp2p-crypto"
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/lazyledger/optimint/config"
+	"github.com/lazyledger/optimint/mocks"
 	"github.com/lazyledger/optimint/node"
 )
 
@@ -23,83 +25,25 @@ var expectedInfo = abci.ResponseInfo{
 	LastBlockHeight: 0,
 }
 
-type MockApp struct {
-}
-
-// Info/Query Connection
-// Return application info
-func (m *MockApp) Info(_ abci.RequestInfo) abci.ResponseInfo {
-	return expectedInfo
-}
-
-func (m *MockApp) Query(_ abci.RequestQuery) abci.ResponseQuery {
-	panic("not implemented") // TODO: Implement
-}
-
-// Mempool Connection
-// Validate a tx for the mempool
-func (m *MockApp) CheckTx(_ abci.RequestCheckTx) abci.ResponseCheckTx {
-	panic("not implemented") // TODO: Implement
-}
-
-// Consensus Connection
-// Initialize blockchain w validators/other info from TendermintCore
-func (m *MockApp) InitChain(_ abci.RequestInitChain) abci.ResponseInitChain {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) BeginBlock(_ abci.RequestBeginBlock) abci.ResponseBeginBlock {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) DeliverTx(_ abci.RequestDeliverTx) abci.ResponseDeliverTx {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) EndBlock(_ abci.RequestEndBlock) abci.ResponseEndBlock {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) Commit() abci.ResponseCommit {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) PreprocessTxs(_ abci.RequestPreprocessTxs) abci.ResponsePreprocessTxs {
-	panic("not implemented") // TODO: Implement
-}
-
-// State Sync Connection
-// List available snapshots
-func (m *MockApp) ListSnapshots(_ abci.RequestListSnapshots) abci.ResponseListSnapshots {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) OfferSnapshot(_ abci.RequestOfferSnapshot) abci.ResponseOfferSnapshot {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) LoadSnapshotChunk(_ abci.RequestLoadSnapshotChunk) abci.ResponseLoadSnapshotChunk {
-	panic("not implemented") // TODO: Implement
-}
-
-func (m *MockApp) ApplySnapshotChunk(_ abci.RequestApplySnapshotChunk) abci.ResponseApplySnapshotChunk {
-	panic("not implemented") // TODO: Implement
-}
-
 func TestInfo(t *testing.T) {
 	assert := assert.New(t)
 
-	rpc := getRPC(t)
+	mockApp, rpc := getRPC(t)
+	mockApp.On("Info", mock.Anything).Return(expectedInfo)
 
 	info, err := rpc.ABCIInfo(context.Background())
 	assert.NoError(err)
 	assert.Equal(expectedInfo, info.Response)
 }
 
-func getRPC(t *testing.T) *Local {
+func TestMempool(t *testing.T) {
+
+}
+
+func getRPC(t *testing.T) (*mocks.Application, *Local) {
 	t.Helper()
 	require := require.New(t)
-	app := &MockApp{}
+	app := &mocks.Application{}
 	key, _, _ := crypto.GenerateEd25519Key(rand.Reader)
 	node, err := node.NewNode(context.Background(), config.NodeConfig{}, key, proxy.NewLocalClientCreator(app), &types.GenesisDoc{}, log.TestingLogger())
 	require.NoError(err)
@@ -108,5 +52,5 @@ func getRPC(t *testing.T) *Local {
 	rpc := NewLocal(node)
 	require.NotNil(rpc)
 
-	return rpc
+	return app, rpc
 }
