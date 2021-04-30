ea091fbfa051b6ebdce0caca69bcb19ccb352e7e
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-04-30 10:02:11 +0200

    Improved block store tests

--- store/blockstore_test.go
+++ store/blockstore_test.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/lazyledger/optimint/types"
@@ -9,37 +10,115 @@ import (
 )
 
 func TestBlockstoreHeight(t *testing.T) {
-	assert := assert.New(t)
+	t.Parallel()
+	cases := []struct {
+		name     string
+		blocks   []*types.Block
+		expected uint64
+	}{
+		{"single block", []*types.Block{getRandomBlock(1, 0)}, 1},
+		{"two consecutive blocks", []*types.Block{
+			getRandomBlock(1, 0),
+			getRandomBlock(2, 0),
+		}, 2},
+		{"blocks out of order", []*types.Block{
+			getRandomBlock(2, 0),
+			getRandomBlock(3, 0),
+			getRandomBlock(1, 0),
+		}, 3},
+		{"with a gap", []*types.Block{
+			getRandomBlock(1, 0),
+			getRandomBlock(9, 0),
+			getRandomBlock(10, 0),
+		}, 10},
+	}
 
-	bstore := NewBlockStore()
-	assert.Equal(uint64(0), bstore.Height())
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			assert := assert.New(t)
+			bstore := NewBlockStore()
+			assert.Equal(uint64(0), bstore.Height())
 
-	err := bstore.SaveBlock(&types.Block{
-		Header: types.Header{
-			Height: 1,
-		},
-	})
-	assert.NoError(err)
-	assert.Equal(uint64(1), bstore.Height())
+			for _, block := range c.blocks {
+				err := bstore.SaveBlock(block)
+				assert.NoError(err)
+			}
+
+			assert.Equal(c.expected, bstore.Height())
+		})
+	}
 }
 
 func TestBlockstoreLoad(t *testing.T) {
-	assert := assert.New(t)
-	require := require.New(t)
+	t.Parallel()
+	cases := []struct {
+		name   string
+		blocks []*types.Block
+	}{
+		{"single block", []*types.Block{getRandomBlock(1, 10)}},
+		{"two consecutive blocks", []*types.Block{
+			getRandomBlock(1, 10),
+			getRandomBlock(2, 20),
+		}},
+		{"blocks out of order", []*types.Block{
+			getRandomBlock(2, 20),
+			getRandomBlock(3, 30),
+			getRandomBlock(4, 100),
+			getRandomBlock(5, 10),
+			getRandomBlock(1, 10),
+		}},
+	}
 
-	bstore := NewBlockStore()
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
 
-	err := bstore.SaveBlock(&types.Block{
+			bstore := NewBlockStore()
+
+			for _, block := range c.blocks {
+				err := bstore.SaveBlock(block)
+				require.NoError(err)
+			}
+
+			for _, expected := range c.blocks {
+				block, err := bstore.LoadBlock(expected.Header.Height)
+				assert.NoError(err)
+				assert.NotNil(block)
+				assert.Equal(expected, block)
+			}
+		})
+	}
+}
+
+func getRandomBlock(height uint64, nTxs int) *types.Block {
+	block := &types.Block{
 		Header: types.Header{
-			Height: 1,
+			Height: height,
 		},
-	})
-	require.NoError(err)
-	assert.Equal(uint64(1), bstore.Height())
+		Data: types.Data{
+			Txs: make(types.Txs, nTxs),
+			IntermediateStateRoots: types.IntermediateStateRoots{
+				RawRootsList: make([][]byte, nTxs),
+			},
+		},
+	}
 
-	block, err := bstore.LoadBlock(1)
-	require.NoError(err)
-	require.NotNil(block)
+	for i := 0; i < nTxs; i++ {
+		block.Data.Txs[i] = getRandomTx()
+		block.Data.IntermediateStateRoots.RawRootsList[i] = getRandomBytes(32)
+	}
 
-	assert.Equal(uint64(1), block.Header.Height)
+	return block
+}
+
+func getRandomTx() types.Tx {
+	size := rand.Int()%100 + 100
+	return types.Tx(getRandomBytes(size))
+}
+
+func getRandomBytes(n int) []byte {
+	data := make([]byte, n)
+	_, _ = rand.Read(data)
+	return data
 }
