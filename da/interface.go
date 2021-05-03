5c20a384dc9abafc68f4ec77056f2574be434d22
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-05-03 23:27:07 +0200

    Add interface for Data Availability layer

---
+++ da/interface.go
@@ -0,0 +1,26 @@
+package da
+
+import "github.com/lazyledger/optimint/types"
+
+// TODO define an enum of different non-happy-path cases
+// that might need to be handled by Optimint independent of
+// the underlying DA chain.
+type StatusCode uint64
+
+type ResultSubmitBlock struct {
+	// Code is to determine if the action succeeded.
+	Code StatusCode
+	// Not sure if this needs to be bubbled up to other
+	// parts of Optimint.
+	// Hash hash.Hash
+}
+
+type DataAvailabilityLayerClient interface {
+	Start() error
+	Stop() error
+
+	// SubmitBlock submits the passed in block to the DA layer.
+	// This should create a transaction which (potentially)
+	// triggers a state transition in the DA layer.
+	SubmitBlock(block *types.Block) ResultSubmitBlock
+}
