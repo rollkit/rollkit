e9aaaa04ab551b88107ae7b226420d2645e0ce74
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-06-11 11:18:21 +0200

    Add comments to interface.

--- store/types.go
+++ store/types.go
@@ -2,14 +2,21 @@ package store
 
 import "github.com/lazyledger/optimint/types"
 
+// Store is minimal interface for storing and retrieving blocks and commits.
 type Store interface {
+	// Height returns height of the highest block in store.
 	Height() uint64
 
+	// SaveBlock saves block along with it's commit.
 	SaveBlock(block *types.Block, commit *types.Commit) error
 
+	// LoadBlock returns block at given height, or error if it's not found in Store.
 	LoadBlock(height uint64) (*types.Block, error)
+	// LoadBlockByHash returns block with given block header hash, or error if it's not found in Store.
 	LoadBlockByHash(hash [32]byte) (*types.Block, error)
 
+	// LoadCommit returns commit for a block at given height, or error if it's not found in Store.
 	LoadCommit(height uint64) (*types.Commit, error)
+	// LoadCommit returns commit for a block with given block header hash, or error if it's not found in Store.
 	LoadCommitByHash(hash [32]byte) (*types.Commit, error)
 }
