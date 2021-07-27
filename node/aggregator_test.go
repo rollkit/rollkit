155191226ee9c4a3e3d20c3878820ff55f62b3fa
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-07-27 12:05:52 +0200

    More aggregator tests

--- node/aggregator_test.go
+++ node/aggregator_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/lazyledger/optimint/state"
+
 	abci "github.com/lazyledger/lazyledger-core/abci/types"
 	"github.com/lazyledger/lazyledger-core/libs/log"
 	"github.com/lazyledger/lazyledger-core/proxy"
@@ -14,6 +16,7 @@ import (
 	"github.com/lazyledger/optimint/config"
 	"github.com/lazyledger/optimint/mocks"
 	"github.com/lazyledger/optimint/p2p"
+	"github.com/lazyledger/optimint/store"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/stretchr/testify/assert"
@@ -69,4 +72,65 @@ func TestAggregatorMode(t *testing.T) {
 	}()
 	time.Sleep(5 * time.Second)
 	cancel()
+}
+
+func TestInitialState(t *testing.T) {
+	genesis := &types.GenesisDoc{
+		ChainID:       "genesis id",
+		InitialHeight: 100,
+	}
+	sampleState := state.State{
+		ChainID:         "state id",
+		InitialHeight:   123,
+		LastBlockHeight: 128,
+	}
+
+	emptyStore := store.New(store.NewInMemoryKVStore())
+
+	fullStore := store.New(store.NewInMemoryKVStore())
+	fullStore.UpdateState(sampleState)
+
+	cases := []struct {
+		name                    string
+		store                   store.Store
+		genesis                 *types.GenesisDoc
+		expectedInitialHeight   int64
+		expectedLastBlockHeight int64
+		expectedChainID         string
+	}{
+		{
+			name:                    "empty store",
+			store:                   emptyStore,
+			genesis:                 genesis,
+			expectedInitialHeight:   genesis.InitialHeight,
+			expectedLastBlockHeight: 0,
+			expectedChainID:         genesis.ChainID,
+		},
+		{
+			name:                    "state in store",
+			store:                   fullStore,
+			genesis:                 genesis,
+			expectedInitialHeight:   sampleState.InitialHeight,
+			expectedLastBlockHeight: sampleState.LastBlockHeight,
+			expectedChainID:         sampleState.ChainID,
+		},
+	}
+
+	key, _, _ := crypto.GenerateEd25519Key(rand.Reader)
+	conf := config.AggregatorConfig{
+		BlockTime:   10 * time.Second,
+		NamespaceID: [8]byte{1, 2, 3, 4, 5, 6, 7, 8},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			assert := assert.New(t)
+			agg, err := newAggregator(key, conf, c.genesis, c.store, nil, nil, nil, log.TestingLogger())
+			assert.NoError(err)
+			assert.NotNil(agg)
+			assert.Equal(c.expectedChainID, agg.lastState.ChainID)
+			assert.Equal(c.expectedInitialHeight, agg.lastState.InitialHeight)
+			assert.Equal(c.expectedLastBlockHeight, agg.lastState.LastBlockHeight)
+		})
+	}
 }
