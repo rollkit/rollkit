38d932a61812464e85aef62685b1074cdd608399
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-09-30 11:15:09 +0200

    Application Data Retrieval API protobuf definitions

---
+++ da/celestia/proto/gen.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# see: https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR
+protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf --gofast_out=../types/pb/celestia celestia.proto
