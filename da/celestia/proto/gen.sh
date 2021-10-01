2cb8febbe429f7004e682ef44c3063d50f00a50b
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-10-01 13:58:32 +0200

    Draft of Celestia Retrieval API

--- da/celestia/proto/gen.sh
+++ da/celestia/proto/gen.sh
@@ -4,4 +4,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd $SCRIPT_DIR
-protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf --gofast_out=../types/pb/celestia celestia.proto
+
+protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf\
+	--gofast_out=plugins=grpc:../pb\
+	celestia.proto 
