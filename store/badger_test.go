b490eca0ddc6853c0c095b1667d2c09c63519800
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2021-10-10 22:01:03 +0200

    chore: add badger test

---
+++ store/badger_test.go
@@ -0,0 +1,30 @@
+package store
+
+import (
+	"testing"
+
+	"github.com/dgraph-io/badger"
+)
+
+func TestGet(t *testing.T) {
+	dalcKV := NewInMemoryKVStore()
+
+	tc := []struct {
+		name string
+		key  []byte
+		err  error
+	}{
+		{"empty key", []byte{}, badger.ErrEmptyKey},
+		{"not found key", []byte("missing key"), ErrKeyNotFound},
+		{"invalid key", []byte("!badger"), badger.ErrInvalidKey},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := dalcKV.Get(tt.key)
+			if err.Error() != tt.err.Error() {
+				t.Errorf("Invalid err, got: %#v expected %#v", err, tt.err)
+			}
+		})
+	}
+}
