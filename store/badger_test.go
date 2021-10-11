3a6ac53aeb8b0b42b51eb606661fbbb14ce88799
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2021-10-11 07:52:54 +0200

    chore: add unit test for Set and Delete also

--- store/badger_test.go
+++ store/badger_test.go
@@ -27,3 +27,48 @@ func TestGet(t *testing.T) {
 		})
 	}
 }
+
+func TestSet(t *testing.T) {
+	dalcKV := NewInMemoryKVStore()
+
+	tc := []struct {
+		name  string
+		key   []byte
+		value []byte
+		err   error
+	}{
+		{"empty key", []byte{}, []byte{}, badger.ErrEmptyKey},
+		{"invalid key", []byte("!badger!key"), []byte("invalid header"), badger.ErrInvalidKey},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			err := dalcKV.Set(tt.key, tt.value)
+			if err.Error() != tt.err.Error() {
+				t.Errorf("Invalid err, got: %v expected %v", err, tt.err)
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	dalcKV := NewInMemoryKVStore()
+
+	tc := []struct {
+		name string
+		key  []byte
+		err  error
+	}{
+		{"empty key", []byte{}, badger.ErrEmptyKey},
+		{"invalid key", []byte("!badger!key"), badger.ErrInvalidKey},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			err := dalcKV.Delete(tt.key)
+			if err.Error() != tt.err.Error() {
+				t.Errorf("Invalid err, got: %v expected %v", err, tt.err)
+			}
+		})
+	}
+}
