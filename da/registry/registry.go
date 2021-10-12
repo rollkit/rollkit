d778f80caa50ec897e08b1e57f047a666673f2f5
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-10-12 13:40:00 +0200

    GRPC DALC and mock server

--- da/registry/registry.go
+++ da/registry/registry.go
@@ -2,12 +2,14 @@ package registry
 
 import (
 	"github.com/celestiaorg/optimint/da"
+	"github.com/celestiaorg/optimint/da/grpc"
 	"github.com/celestiaorg/optimint/da/mock"
 )
 
 // this is a central registry for all Data Availability Layer Clients
 var clients = map[string]func() da.DataAvailabilityLayerClient{
 	"mock": func() da.DataAvailabilityLayerClient { return &mock.MockDataAvailabilityLayerClient{} },
+	"grpc": func() da.DataAvailabilityLayerClient { return &grpc.DataAvailabilityLayerClient{} },
 }
 
 // GetClient returns client identified by name.
