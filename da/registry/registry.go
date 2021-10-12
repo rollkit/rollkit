01bed4eb5f45604373f53e35c2566cf4361c962f
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-10-12 09:43:31 +0200

    Remove celestia DALC

--- da/registry/registry.go
+++ da/registry/registry.go
@@ -2,14 +2,12 @@ package registry
 
 import (
 	"github.com/celestiaorg/optimint/da"
-	"github.com/celestiaorg/optimint/da/celestia"
 	"github.com/celestiaorg/optimint/da/mock"
 )
 
 // this is a central registry for all Data Availability Layer Clients
 var clients = map[string]func() da.DataAvailabilityLayerClient{
-	"mock":     func() da.DataAvailabilityLayerClient { return &mock.MockDataAvailabilityLayerClient{} },
-	"celestia": func() da.DataAvailabilityLayerClient { return &celestia.Celestia{} },
+	"mock": func() da.DataAvailabilityLayerClient { return &mock.MockDataAvailabilityLayerClient{} },
 }
 
 // GetClient returns client identified by name.
