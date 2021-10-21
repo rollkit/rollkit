4db5b9fe8fa3174d2461d326e72e5ea62685395a
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-10-21 21:29:38 +0200

    gRPC DALC and mock server (#158)
    * Remove celestia DALC
    * DALC gRPC definitions
    * GRPC DALC and mock server
    * Generalized DALC tests

--- da/registry/registry.go
+++ da/registry/registry.go
@@ -2,17 +2,29 @@ package registry
 
 import (
 	"github.com/celestiaorg/optimint/da"
-	"github.com/celestiaorg/optimint/da/celestia"
+	"github.com/celestiaorg/optimint/da/grpc"
 	"github.com/celestiaorg/optimint/da/mock"
 )
 
 // this is a central registry for all Data Availability Layer Clients
 var clients = map[string]func() da.DataAvailabilityLayerClient{
-	"mock":     func() da.DataAvailabilityLayerClient { return &mock.MockDataAvailabilityLayerClient{} },
-	"celestia": func() da.DataAvailabilityLayerClient { return &celestia.Celestia{} },
+	"mock": func() da.DataAvailabilityLayerClient { return &mock.MockDataAvailabilityLayerClient{} },
+	"grpc": func() da.DataAvailabilityLayerClient { return &grpc.DataAvailabilityLayerClient{} },
 }
 
 // GetClient returns client identified by name.
 func GetClient(name string) da.DataAvailabilityLayerClient {
-	return clients[name]()
+	f, ok := clients[name]
+	if !ok {
+		return nil
+	}
+	return f()
+}
+
+func RegisteredClients() []string {
+	registered := make([]string, 0, len(clients))
+	for name := range clients {
+		registered = append(registered, name)
+	}
+	return registered
 }
---
+++ da/registry/registry_test.go
@@ -0,0 +1,23 @@
+package registry
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegistery(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := []string{"mock", "grpc"}
+	actual := RegisteredClients()
+
+	assert.ElementsMatch(expected, actual)
+
+	for _, e := range expected {
+		dalc := GetClient(e)
+		assert.NotNil(dalc)
+	}
+
+	assert.Nil(GetClient("nonexistent"))
+}
