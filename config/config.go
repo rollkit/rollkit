a6ffe62e55dd3f52b1d440c78bdba4cec5c49e99
Tomasz Zdybał <tomek@zdybal.lap.pl>
2021-10-28 13:15:20 +0200

    gofmt

--- config/config.go
+++ config/config.go
@@ -5,15 +5,14 @@ import (
 	"time"
 )
 
-
 // NodeConfig stores Optimint node configuration.
 type NodeConfig struct {
 	// parameters below are translated from existing config
-	RootDir            string
-	DBPath             string
-	P2P                P2PConfig
+	RootDir string
+	DBPath  string
+	P2P     P2PConfig
 	// parameters below are optimint specific and read from config
-	Aggregator         bool      `mapstructure:"aggregator"`
+	Aggregator         bool `mapstructure:"aggregator"`
 	BlockManagerConfig `mapstructure:",squash"`
 	DALayer            string `mapstructure:"da_layer"`
 	DAConfig           string `mapstructure:"da_config"`
@@ -26,7 +25,7 @@ type BlockManagerConfig struct {
 }
 
 func (nc *NodeConfig) GetViperConfig(v *viper.Viper) {
-	_ =  v.UnmarshalKey("optimint", nc)
+	_ = v.UnmarshalKey("optimint", nc)
 }
 
 /*
@@ -38,4 +37,4 @@ func AddFlags(cmd *cobra.Command) {
 	cmd.Flags().Duration("optimint.block_time", 15*time.Second, "block time (for aggregator mode)")
 	cmd.Flags().BytesHex("optimint.namespace_id", nil, "namespace identifies (8 bytes in hex)")
 }
- */
+*/
