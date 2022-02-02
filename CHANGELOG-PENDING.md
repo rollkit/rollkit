39e405084cabe83ea86475d1ddd46ba1528d7353
Tomasz Zdybał <tomek@zdybal.lap.pl>
2022-02-02 10:29:17 +0100

    WebSocket support and subscription methods (#252)
    Resolves: #188 
    Resolves: #244

--- CHANGELOG-PENDING.md
+++ CHANGELOG-PENDING.md
@@ -12,11 +12,12 @@ Month, DD, YYYY
 
 - [indexer] [Implement block and transaction indexing, enable TxSearch RPC endpoint #202](https://github.com/celestiaorg/optimint/pull/202) [@mattdf](https://github.com/mattdf)
 - [rpc] [Tendermint URI RPC #224](https://github.com/celestiaorg/optimint/pull/224) [@tzdybal](https://github.com/tzdybal/)
+- [rpc] [Subscription methods #252](https://github.com/celestiaorg/optimint/pull/252) [@tzdybal](https://github.com/tzdybal/)
 
 ### IMPROVEMENTS
 
 - [ci] [Add more linters #219](https://github.com/celestiaorg/optimint/pull/219) [@tzdybal](https://github.com/tzdybal/)
-- [deps] [Update dependencies: grpc, cors, cobra, viper, tm-db #245](https://github.com/celestiaorg/optimint/pull/245)  [@tzdybal](https://github.com/tzdybal/)
+- [deps] [Update dependencies: grpc, cors, cobra, viper, tm-db #245](https://github.com/celestiaorg/optimint/pull/245) [@tzdybal](https://github.com/tzdybal/)
 - [rpc] [Implement NumUnconfirmedTxs #255](https://github.com/celestiaorg/optimint/pull/255) [@tzdybal](https://github.com/tzdybal/)
 - [rpc] [Implement BlockByHash #256](https://github.com/celestiaorg/optimint/pull/256) [@mauriceLC92](https://github.com/mauriceLC92)
 - [rpc] [Implement BlockResults #263](https://github.com/celestiaorg/optimint/pull/263) [@tzdybal](https://github.com/tzdybal/)
--- rpc/json/handler.go
+++ rpc/json/handler.go
@@ -10,43 +10,50 @@ import (
 	"reflect"
 	"strconv"
 
+	"github.com/celestiaorg/optimint/log"
 	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
-
-	"github.com/celestiaorg/optimint/log"
 )
 
 type handler struct {
-	s *service
-	m *http.ServeMux
-	c rpc.Codec
-	l log.Logger
+	srv    *service
+	mux    *http.ServeMux
+	codec  rpc.Codec
+	logger log.Logger
 }
 
 func newHandler(s *service, codec rpc.Codec, logger log.Logger) *handler {
 	mux := http.NewServeMux()
 	h := &handler{
-		m: mux,
-		s: s,
-		c: codec,
-		l: logger,
+		srv:    s,
+		mux:    mux,
+		codec:  codec,
+		logger: logger,
 	}
+
 	mux.HandleFunc("/", h.serveJSONRPC)
+	mux.HandleFunc("/websocket", h.wsHandler)
 	for name, method := range s.methods {
 		logger.Debug("registering method", "name", name)
 		mux.HandleFunc("/"+name, h.newHandler(method))
 	}
+
 	return h
 }
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.m.ServeHTTP(w, r)
+	h.mux.ServeHTTP(w, r)
+}
+
+// serveJSONRPC serves HTTP request
+func (h *handler) serveJSONRPC(w http.ResponseWriter, r *http.Request) {
+	h.serveJSONRPCforWS(w, r, nil)
 }
 
 // serveJSONRPC serves HTTP request
 // implementation is highly inspired by Gorilla RPC v2 (but simplified a lot)
-func (h *handler) serveJSONRPC(w http.ResponseWriter, r *http.Request) {
-	// Create a new c request.
-	codecReq := h.c.NewRequest(r)
+func (h *handler) serveJSONRPCforWS(w http.ResponseWriter, r *http.Request, wsConn *wsConn) {
+	// Create a new codec request.
+	codecReq := h.codec.NewRequest(r)
 	// Get service method to be called.
 	method, err := codecReq.Method()
 	if err != nil {
@@ -57,7 +64,7 @@ func (h *handler) serveJSONRPC(w http.ResponseWriter, r *http.Request) {
 		codecReq.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
-	methodSpec, ok := h.s.methods[method]
+	methodSpec, ok := h.srv.methods[method]
 	if !ok {
 		codecReq.WriteError(w, int(json2.E_NO_METHOD), err)
 		return
@@ -70,10 +77,14 @@ func (h *handler) serveJSONRPC(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rets := methodSpec.m.Call([]reflect.Value{
+	callArgs := []reflect.Value{
 		reflect.ValueOf(r),
 		args,
-	})
+	}
+	if methodSpec.ws {
+		callArgs = append(callArgs, reflect.ValueOf(wsConn))
+	}
+	rets := methodSpec.m.Call(callArgs)
 
 	// Extract the result to error if needed.
 	var errResult error
@@ -171,7 +182,7 @@ func (h *handler) encodeAndWriteResponse(w http.ResponseWriter, result interface
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(resp)
 	if err != nil {
-		h.l.Error("failed to encode RPC response", "error", err)
+		h.logger.Error("failed to encode RPC response", "error", err)
 	}
 }
 
