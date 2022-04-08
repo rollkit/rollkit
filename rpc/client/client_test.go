endygamie/optimint

Unit test for Evmos indexing issue

rpc/client/client_test.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	crand "crypto/rand"
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -331,6 +332,61 @@ func TestUnconfirmedTxsLimit(t *testing.T) {
 	assert.Len(txRes.Txs, limit)
 	assert.Contains(txRes.Txs, tx1)
 	assert.NotContains(txRes.Txs, tx2)
+}
+
+func TestEventIndexing(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	mockApp := &mocks.Application{}
+	mockApp.On("InitChain", mock.Anything).Return(abci.ResponseInitChain{})
+	key, _, _ := crypto.GenerateEd25519Key(crand.Reader)
+	node, err := node.NewNode(context.Background(), config.NodeConfig{
+		DALayer:    "mock",
+		Aggregator: true,
+		BlockManagerConfig: config.BlockManagerConfig{
+			BlockTime: 1 * time.Second,
+		}},
+		key, proxy.NewLocalClientCreator(mockApp),
+		&tmtypes.GenesisDoc{ChainID: "test"},
+		log.TestingLogger())
+	require.NoError(err)
+	require.NotNil(node)
+
+	rpc := NewClient(node)
+	require.NotNil(rpc)
+	mockApp.On("BeginBlock", mock.Anything).Return(abci.ResponseBeginBlock{})
+	mockApp.On("EndBlock", mock.Anything).Return(abci.ResponseEndBlock{})
+	mockApp.On("Commit", mock.Anything).Return(abci.ResponseCommit{})
+	const evType = "ethereum_tx"
+	const evKey = "ethereumTxHash"
+	const evValue = "RLP encoded Keccak hash"
+	mockApp.On("DeliverTx", mock.Anything).Return(abci.ResponseDeliverTx{
+		Events: []abci.Event{{
+			Type: evType,
+			Attributes: []abci.EventAttribute{{
+				Key:   []byte(evKey),
+				Value: []byte(evValue),
+				Index: true,
+			}},
+		}},
+	})
+	mockApp.On("CheckTx", mock.Anything).Return(abci.ResponseCheckTx{})
+
+	err = rpc.node.Start()
+	require.NoError(err)
+
+	tx1 := tmtypes.Tx("tx1")
+
+	res, err := rpc.BroadcastTxCommit(context.Background(), tx1)
+	assert.NoError(err)
+	assert.NotNil(res)
+
+	time.Sleep(1 * time.Second)
+
+	txs, err := rpc.TxSearch(context.Background(), fmt.Sprintf("%s.%s='%s'", evType, evKey, evValue), false, nil, nil, "asc")
+	assert.NoError(err)
+	assert.EqualValues(1, txs.TotalCount)
 }
 
 // copy-pasted from store/store_test.go

rollkit/rollkit

rpc/client/client_test.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 	crand "crypto/rand"
-	cryptorand "crypto/rand"
 	"fmt"
 	"math/rand"
 	"testing"
@@ -90,8 +89,8 @@ func TestGenesisChunked(t *testing.T) {
 
 	mockApp := &mocks.Application{}
 	mockApp.On("InitChain", mock.Anything).Return(abci.ResponseInitChain{})
-	privKey, _, _ := crypto.GenerateEd25519Key(cryptorand.Reader)
-	signingKey, _, _ := crypto.GenerateEd25519Key(cryptorand.Reader)
+	privKey, _, _ := crypto.GenerateEd25519Key(crand.Reader)
+	signingKey, _, _ := crypto.GenerateEd25519Key(crand.Reader)
 	n, _ := node.NewNode(context.Background(), config.NodeConfig{DALayer: "mock"}, privKey, signingKey, proxy.NewLocalClientCreator(mockApp), genDoc, log.TestingLogger())
 
 	rpc := NewClient(n)
@@ -535,6 +534,63 @@ func TestUnconfirmedTxsLimit(t *testing.T) {
 	assert.Len(txRes.Txs, limit)
 	assert.Contains(txRes.Txs, tx1)
 	assert.NotContains(txRes.Txs, tx2)
+}
+
+func TestEventIndexing(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	mockApp := &mocks.Application{}
+	mockApp.On("InitChain", mock.Anything).Return(abci.ResponseInitChain{})
+	p2pKey, _, _ := crypto.GenerateEd25519Key(crand.Reader)
+	signingKey, _, _ := crypto.GenerateEd25519Key(crand.Reader)
+
+	node, err := node.NewNode(context.Background(), config.NodeConfig{
+		DALayer:    "mock",
+		Aggregator: true,
+		BlockManagerConfig: config.BlockManagerConfig{
+			BlockTime: 1 * time.Second,
+		}},
+		p2pKey, signingKey, proxy.NewLocalClientCreator(mockApp),
+		&tmtypes.GenesisDoc{ChainID: "test"},
+		log.TestingLogger())
+	require.NoError(err)
+	require.NotNil(node)
+
+	rpc := NewClient(node)
+	require.NotNil(rpc)
+	mockApp.On("BeginBlock", mock.Anything).Return(abci.ResponseBeginBlock{})
+	mockApp.On("EndBlock", mock.Anything).Return(abci.ResponseEndBlock{})
+	mockApp.On("Commit", mock.Anything).Return(abci.ResponseCommit{})
+	const evType = "ethereum_tx"
+	const evKey = "ethereumTxHash"
+	const evValue = "RLP encoded Keccak hash"
+	mockApp.On("DeliverTx", mock.Anything).Return(abci.ResponseDeliverTx{
+		Events: []abci.Event{{
+			Type: evType,
+			Attributes: []abci.EventAttribute{{
+				Key:   []byte(evKey),
+				Value: []byte(evValue),
+				Index: true,
+			}},
+		}},
+	})
+	mockApp.On("CheckTx", mock.Anything).Return(abci.ResponseCheckTx{})
+
+	err = rpc.node.Start()
+	require.NoError(err)
+
+	tx1 := tmtypes.Tx("tx1")
+
+	res, err := rpc.BroadcastTxCommit(context.Background(), tx1)
+	assert.NoError(err)
+	assert.NotNil(res)
+
+	time.Sleep(1 * time.Second)
+
+	txs, err := rpc.TxSearch(context.Background(), fmt.Sprintf("%s.%s='%s'", evType, evKey, evValue), false, nil, nil, "asc")
+	assert.NoError(err)
+	assert.EqualValues(1, txs.TotalCount)
 }
 
 func TestConsensusState(t *testing.T) {