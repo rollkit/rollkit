9113e6933de620508b0e6be40a26a6fca8f2a24d
Tomasz Zdybał <tomek@zdybal.lap.pl>
2022-06-09 21:57:01 +0200

    fix: apply review suggestions

--- proto/get_deps.sh
+++ proto/get_deps.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# see: https://stackoverflow.com/a/246128
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 TM_VERSION=v0.34.14
 TM_PROTO_URL=https://raw.githubusercontent.com/tendermint/tendermint/$TM_VERSION/proto/tendermint
-GOGO_PROTO_URL=https://raw.githubusercontent.com/regen-network/protobuf/cosmos
 
 TM_PROTO_FILES=(
   abci/types.proto
@@ -23,12 +20,8 @@ TM_PROTO_FILES=(
 )
 
 echo Fetching protobuf dependencies from Tendermint $TM_VERSION
-for FILE in "${TM_PROTO_FILES[@]}";
-do
-  echo Fetching $FILE
-  mkdir -p tendermint/`dirname $FILE`
+for FILE in "${TM_PROTO_FILES[@]}"; do
+  echo Fetching "$FILE"
+  mkdir -p "tendermint/$(dirname $FILE)"
   curl -sSL "$TM_PROTO_URL/$FILE" > "tendermint/$FILE"
-  #GO_PACKAGE="github.com/celestiaorg/optimint/types/pb/tendermint/`dirname $FILE`"
-  #echo Fixing go_package to $GO_PACKAGE
-  #sed -i -e 's|option go_package = ".*";|option go_package = "'"$GO_PACKAGE"'";|' "tendermint/$FILE"
 done
