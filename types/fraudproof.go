5ef7ab39f1fc96b5f6bea6e40fe2d589ca540c31
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-10-09 19:59:30 -0400

    Revert fraudproof type to remove serialization errors

--- types/fraudproof.go
+++ types/fraudproof.go
@@ -1,38 +1,16 @@
 package types
 
-import (
-	abci "github.com/tendermint/tendermint/abci/types"
-	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
-)
-
 // Represents a single-round fraudProof
 type FraudProof struct {
-	// The block height to load state of
-	blockHeight int64
-
-	appHash []byte
-	// A map from module name to state witness
-	stateWitness map[string]StateWitness
-
-	// Fraudulent state transition has to be one of these
-	// Only one have of these three can be non-nil
-	fraudulentBeginBlock *abci.RequestBeginBlock
-	fraudulentDeliverTx  *abci.RequestDeliverTx
-	fraudulentEndBlock   *abci.RequestEndBlock
+	BlockHeight  uint64
+	StateWitness StateWitness
 }
 
-// State witness with a list of all witness data
 type StateWitness struct {
-	// store level proof
-	Proof    tmcrypto.ProofOp
-	RootHash []byte
-	// List of witness data
 	WitnessData []WitnessData
 }
 
-// Witness data containing a key/value pair and a SMT proof for said key/value pair
 type WitnessData struct {
 	Key   []byte
 	Value []byte
-	Proof tmcrypto.ProofOp
 }
