5689e32c00fc3ece005aaa7e6c4dca13e2d03c3d
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2022-11-22 22:48:17 +0530

    add flag to enable fraud proofs
    minor fix to fix the integration tests
    
    expanding the executor tests to cover both fraud proof enable and disable cases

--- block/manager.go
+++ block/manager.go
@@ -115,7 +115,7 @@ func NewManager(
 		conf.DABlockTime = defaultDABlockTime
 	}
 
-	exec := state.NewBlockExecutor(proposerAddress, conf.NamespaceID, genesis.ChainID, mempool, proxyApp, eventBus, logger)
+	exec := state.NewBlockExecutor(proposerAddress, conf.NamespaceID, genesis.ChainID, mempool, proxyApp, conf.FraudProofs, eventBus, logger)
 	if s.LastBlockHeight+1 == genesis.InitialHeight {
 		res, err := exec.InitChain(genesis)
 		if err != nil {
