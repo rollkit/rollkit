a0c9a40ccb5f7f79341367d5ddebe182e6e2c7ad
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2022-12-08 10:29:30 +0530

    fix linter bug, remove unwanted test, add lastheader test

--- types/header_test.go
+++ types/header_test.go
@@ -1,81 +1,11 @@
 package types
 
 import (
-	"crypto/rand"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func TestInterfaceCompatible1(t *testing.T) {
-	t.Parallel()
-
-	require := require.New(t)
-
-	// create random hashes
-	h := [][32]byte{}
-	for i := 0; i < 8; i++ {
-		var h1 [32]byte
-		n, err := rand.Read(h1[:])
-		require.Equal(32, n)
-		require.NoError(err)
-		h = append(h, h1)
-	}
-
-	cases := []struct {
-		name  string
-		input *Header
-	}{
-		{"empty", &Header{}},
-		{"first", &Header{
-			Version: Version{
-				Block: 1,
-				App:   2,
-			},
-			NamespaceID:     NamespaceID{0, 1, 2, 3, 4, 5, 6, 7},
-			Height:          3,
-			Time:            4567,
-			LastHeaderHash:  h[0],
-			LastCommitHash:  h[1],
-			DataHash:        h[2],
-			ConsensusHash:   h[3],
-			AppHash:         h[4],
-			LastResultsHash: h[5],
-			ProposerAddress: []byte{4, 3, 2, 1},
-			AggregatorsHash: h[6],
-		},
-		},
-		{"next", &Header{
-			Version: Version{
-				Block: 1,
-				App:   2,
-			},
-			NamespaceID:     NamespaceID{0, 1, 2, 3, 4, 5, 6, 7},
-			Height:          4,
-			Time:            4567,
-			LastHeaderHash:  h[0],
-			LastCommitHash:  h[1],
-			DataHash:        h[2],
-			ConsensusHash:   h[3],
-			AppHash:         h[4],
-			LastResultsHash: h[5],
-			ProposerAddress: []byte{4, 3, 2, 1},
-			AggregatorsHash: h[6],
-		},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			assert := assert.New(t)
-			var h H = c.input
-			_, ok := h.(H)
-			assert.True(ok)
-		})
-	}
-}
 
 type A struct {
 }
@@ -165,7 +95,11 @@ func TestNumber(t *testing.T) {
 }
 
 func TestLastHeader(t *testing.T) {
+	assert := assert.New(t)
+	h := [32]byte{104, 232, 90, 84, 228, 141, 242, 116, 213, 125, 19, 72, 23, 49, 5, 255, 5, 82, 174, 209, 213, 171, 106, 106, 156, 227, 119, 225, 24, 130, 129, 185}
+	h1 := &Header{LastHeaderHash: h}
 
+	assert.Equal(h1.LastHeader(), h)
 }
 
 func TestTimestamp(t *testing.T) {
