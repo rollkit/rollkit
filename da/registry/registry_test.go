3146a211359fe62eb23052a8fecb4711599e79e8
Tomasz Zdybał <tomek@zdybal.lap.pl>
2023-01-31 10:33:25 +0100

    feat: add Register function to DA registry

--- da/registry/registry_test.go
+++ da/registry/registry_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/rollkit/rollkit/da"
+	"github.com/rollkit/rollkit/da/mock"
 )
 
 func TestRegistery(t *testing.T) {
@@ -14,7 +17,21 @@ func TestRegistery(t *testing.T) {
 
 	assert.ElementsMatch(expected, actual)
 
-	for _, e := range expected {
+	constructor := func() da.DataAvailabilityLayerClient {
+		return &mock.DataAvailabilityLayerClient{} // cheating, only for tests :D
+	}
+	err := Register("testDA", constructor)
+	assert.NoError(err)
+
+	// re-registration should fail
+	err = Register("celestia", constructor)
+	regErr := &ErrAlreadyRegistered{}
+	assert.ErrorAs(err, &regErr)
+	assert.Equal("celestia", regErr.name)
+
+	assert.Contains(RegisteredClients(), "testDA")
+
+	for _, e := range RegisteredClients() {
 		dalc := GetClient(e)
 		assert.NotNil(dalc)
 	}
