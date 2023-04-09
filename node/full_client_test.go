14e16416be9126d427bc88d36b59168f3de65e09
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-04-09 18:58:32 +0530

    Based rollups work now

--- node/full_client_test.go
+++ node/full_client_test.go
@@ -663,7 +663,7 @@ func TestValidatorSetHandling(t *testing.T) {
 	for i := 0; i < len(vKeys); i++ {
 		vKeys[i] = ed25519.GenPrivKey()
 		genesisValidators[i] = tmtypes.GenesisValidator{Address: vKeys[i].PubKey().Address(), PubKey: vKeys[i].PubKey(), Power: int64(i + 100), Name: fmt.Sprintf("gen #%d", i)}
-		apps[i] = createApp1(vKeys[0], waitCh, require)
+		apps[i] = createApp(vKeys[0], waitCh, require)
 	}
 
 	dalc := &mockda.DataAvailabilityLayerClient{}
@@ -756,7 +756,7 @@ func TestValidatorSetHandling(t *testing.T) {
 	assert.GreaterOrEqual(vals.BlockHeight, int64(9))
 }
 
-func createApp1(keyToRemove tmcrypto.PrivKey, waitCh chan interface{}, require *require.Assertions) *mocks.Application {
+func createApp(keyToRemove tmcrypto.PrivKey, waitCh chan interface{}, require *require.Assertions) *mocks.Application {
 	app := &mocks.Application{}
 	app.On("InitChain", mock.Anything).Return(abci.ResponseInitChain{})
 	app.On("CheckTx", mock.Anything).Return(abci.ResponseCheckTx{})
@@ -797,7 +797,7 @@ func TestValidatorSetHandlingBased(t *testing.T) {
 	}
 
 	for i := 0; i < len(vKeys); i++ {
-		apps[i] = createApp2(vKeys, waitCh, require)
+		apps[i] = createAppBased(vKeys, waitCh, require)
 	}
 
 	dalc := &mockda.DataAvailabilityLayerClient{}
@@ -858,17 +858,8 @@ func TestValidatorSetHandlingBased(t *testing.T) {
 		assert.EqualValues(vals.BlockHeight, h)
 	}
 
-	// 3rd EndBlock removes the first validator from the list and makes the rollup based
-	for h := int64(4); h <= 5; h++ {
-		vals, err := rpc.Validators(context.Background(), &h, nil, nil)
-		assert.NoError(err)
-		assert.NotNil(vals)
-		assert.EqualValues(len(genesisValidators)-1, vals.Total)
-		assert.Len(vals.Validators, len(genesisValidators)-1)
-		assert.EqualValues(vals.BlockHeight, h)
-	}
-
-	for h := int64(6); h <= 9; h++ {
+	// 3rd EndBlock removes the first validator and makes the rollup based
+	for h := int64(4); h <= 9; h++ {
 		<-waitCh
 		vals, err := rpc.Validators(context.Background(), &h, nil, nil)
 		assert.NoError(err)
@@ -879,15 +870,16 @@ func TestValidatorSetHandlingBased(t *testing.T) {
 	}
 
 	// check for "latest block"
+	<-waitCh
 	vals, err := rpc.Validators(context.Background(), nil, nil, nil)
 	assert.NoError(err)
 	assert.NotNil(vals)
-	assert.EqualValues(len(genesisValidators), vals.Total)
-	assert.Len(vals.Validators, len(genesisValidators))
+	assert.EqualValues(len(genesisValidators)-1, vals.Total)
+	assert.Len(vals.Validators, len(genesisValidators)-1)
 	assert.GreaterOrEqual(vals.BlockHeight, int64(9))
 }
 
-func createApp2(vKeys []tmcrypto.PrivKey, waitCh chan interface{}, require *require.Assertions) *mocks.Application {
+func createAppBased(vKeys []tmcrypto.PrivKey, waitCh chan interface{}, require *require.Assertions) *mocks.Application {
 	app := &mocks.Application{}
 	app.On("InitChain", mock.Anything).Return(abci.ResponseInitChain{})
 	app.On("CheckTx", mock.Anything).Return(abci.ResponseCheckTx{})
@@ -902,7 +894,6 @@ func createApp2(vKeys []tmcrypto.PrivKey, waitCh chan interface{}, require *requ
 
 	app.On("EndBlock", mock.Anything).Return(abci.ResponseEndBlock{}).Times(2)
 	app.On("EndBlock", mock.Anything).Return(abci.ResponseEndBlock{ValidatorUpdates: []abci.ValidatorUpdate{{PubKey: pbValKey, Power: 0}}}).Once()
-	app.On("EndBlock", mock.Anything).Return(abci.ResponseEndBlock{}).Times(1)
 	app.On("EndBlock", mock.Anything).Return(abci.ResponseEndBlock{}).Run(func(args mock.Arguments) {
 		waitCh <- nil
 	})
