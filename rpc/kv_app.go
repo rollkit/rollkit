4b407605b34ddffcd147183431d3e60b03c5fbe2
Connor O'Hara <connor@switchboard.xyz>
2023-04-30 19:19:11 -0400

    ran into a cometbft / tendermint issue

--- rpc/kv_app.go
+++ rpc/kv_app.go
@@ -1,24 +1,68 @@
 package rpc
 
 import (
+	"bytes"
+
+	"cosmossdk.io/log"
 	"cosmossdk.io/store/iavl"
-	//storetypes "cosmossdk.io/store/types"
-	tmdb "github.com/tendermint/db"
-	"github.com/tendermint/libs/log"
+	"cosmossdk.io/store/metrics"
+	storetypes "cosmossdk.io/store/types"
+	cmdb "github.com/cosmos/cosmos-db"
+	"github.com/tendermint/tendermint/abci/example/code"
 	"github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/libs/log"
 )
 
 var _ types.Application = (*Application)(nil)
 
 type Application struct {
 	types.BaseApplication
-	store iavl.Store
+	store   storetypes.CommitKVStore
+	appHash []byte
 }
 
-func NewMerkleApp() Application {
-	db := tmdb.NewMemDB()
-	log := log.NewNopLogger()
-	store := iavl.LoadStore()
-	return Application{}
+func NewMerkleApp(log log.Logger) Application {
+	db := cmdb.NewMemDB()
+	key := storetypes.NewKVStoreKey("data")
+	commitID := storetypes.CommitID{
+		Version: 0,
+		Hash:    []byte(""),
+	}
+	metrics := metrics.NewNoOpMetrics()
+	store, err := iavl.LoadStore(db, log, key, commitID, false, 10, true, metrics)
+	if err != nil {
+		panic("Unable to create IAVL store")
+	}
+	appHash := make([]byte, 8)
+	return Application{
+		store:   store,
+		appHash: appHash,
+	}
+}
+
+func (a *Application) DeliverTx(req types.RequestDeliverTx) types.ResponseDeliverTx {
+
+	var key, value string
+	parts := bytes.Split(req.Tx, []byte("="))
+	if len(parts) == 2 {
+		key, value = string(parts[0]), string(parts[1])
+	} else {
+		key, value = string(req.Tx), string(req.Tx)
+	}
+	a.store.Set([]byte(key), []byte(value))
+
+	return types.ResponseDeliverTx{
+		Code: code.CodeTypeOK,
+	}
+}
+
+func (a *Application) Commit() types.ResponseCommit {
+	resp := a.store.Commit()
+	a.appHash = resp.Hash
+	return types.ResponseCommit{
+		Data: resp.Hash,
+	}
+}
+
+func (a *Application) Query(req types.RequestQuery) types.ResponseQuery {
+	return a.store.(*iavl.Store).Query(req)
 }
