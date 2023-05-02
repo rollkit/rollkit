6ed20a2814acba263e0f7497938369cc5d41bb30
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2023-05-02 11:37:31 -0700

    fix failing test by making sure that the header and data  namespace ids are not same

--- block/manager_test.go
+++ block/manager_test.go
@@ -94,7 +94,7 @@ func TestInitialState(t *testing.T) {
 
 func getMockDALC(logger log.Logger) da.DataAvailabilityLayerClient {
 	dalc := &mockda.DataAvailabilityLayerClient{}
-	_ = dalc.Init([8]byte{}, [8]byte{}, nil, nil, logger)
+	_ = dalc.Init([8]byte{1, 2, 3, 4, 5, 6, 7, 8}, [8]byte{8, 7, 6, 5, 4, 3, 2, 1}, nil, nil, logger)
 	_ = dalc.Start()
 	return dalc
 }
