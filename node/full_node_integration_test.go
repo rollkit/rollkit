a4da539bfafec9adfa9f5074f4c3919f46f979e8
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-05-17 17:54:04 -0400

    add light node initial height comment

--- node/full_node_integration_test.go
+++ node/full_node_integration_test.go
@@ -565,6 +565,7 @@ func createNode(ctx context.Context, n int, isMalicious bool, aggregator bool, i
 	genesisValidators, signingKey := getGenesisValidatorSetWithSigner(1)
 	genesis := &tmtypes.GenesisDoc{ChainID: "test", Validators: genesisValidators}
 	if isLight {
+		// TODO: need to investigate why this needs to be done for light nodes
 		genesis.InitialHeight = 1
 	}
 	node, err := NewNode(
