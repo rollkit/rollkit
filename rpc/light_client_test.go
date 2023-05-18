1c1771696ccc760bc606378b4ae892ec469c1657
Connor O'Hara <connor@switchboard.xyz>
2023-05-18 17:13:17 -0400

    doing abci query test with code from light client header test

--- rpc/light_client_test.go
+++ rpc/light_client_test.go
@@ -2,32 +2,111 @@ package rpc
 
 import (
 	"context"
-	"crypto/rand"
-	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+
+	//"crypto/rand"
+	//"fmt"
 	"testing"
 	"time"
 
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	//storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/rollkit/rollkit/config"
+	abcitypes "github.com/tendermint/tendermint/abci/types"
+
 	"github.com/rollkit/rollkit/conv"
 	rollnode "github.com/rollkit/rollkit/node"
 	"github.com/rollkit/rollkit/types"
-	"github.com/stretchr/testify/assert"
+
+	//"github.com/stretchr/testify/assert"
+
 	"github.com/stretchr/testify/require"
-	tmconfig "github.com/tendermint/tendermint/config"
+
+	//tmconfig "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	"github.com/tendermint/tendermint/crypto/merkle"
+	//"github.com/tendermint/tendermint/crypto/merkle"
 	"github.com/tendermint/tendermint/libs/log"
 	"github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/proxy"
-	tmclient "github.com/tendermint/tendermint/rpc/client"
+
+	//tmclient "github.com/tendermint/tendermint/rpc/client"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 var genesisValidatorKey = ed25519.GenPrivKey()
 
+func createNode(ctx context.Context, n int, isMalicious bool, aggregator bool, isLight bool, keys []crypto.PrivKey, wg *sync.WaitGroup, t *testing.T) (rollnode.Node, abcitypes.Application) {
+	t.Helper()
+	require := require.New(t)
+	// nodes will listen on consecutive ports on local interface
+	// random connections to other nodes will be added
+	startPort := 10000
+	p2pConfig := config.P2PConfig{
+		ListenAddress: "/ip4/127.0.0.1/tcp/" + strconv.Itoa(startPort+n),
+	}
+	bmConfig := config.BlockManagerConfig{
+		DABlockTime: 100 * time.Millisecond,
+		BlockTime:   1 * time.Second, // blocks must be at least 1 sec apart for adjacent headers to get verified correctly
+		NamespaceID: types.NamespaceID{8, 7, 6, 5, 4, 3, 2, 1},
+		FraudProofs: true,
+	}
+	for i := 0; i < len(keys); i++ {
+		if i == n {
+			continue
+		}
+		r := i
+		id, err := peer.IDFromPrivateKey(keys[r])
+		require.NoError(err)
+		p2pConfig.Seeds += "/ip4/127.0.0.1/tcp/" + strconv.Itoa(startPort+r) + "/p2p/" + id.Pretty() + ","
+	}
+	p2pConfig.Seeds = strings.TrimSuffix(p2pConfig.Seeds, ",")
+
+	app := NewMerkleApp()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	genesisValidators, signingKey := getGenesisValidatorSetWithSigner(1)
+	genesis := &tmtypes.GenesisDoc{ChainID: "test", Validators: genesisValidators}
+	// TODO: need to investigate why this needs to be done for light nodes
+	genesis.InitialHeight = 1
+	node, err := rollnode.NewNode(
+		ctx,
+		config.NodeConfig{
+			P2P:                p2pConfig,
+			DALayer:            "mock",
+			Aggregator:         aggregator,
+			BlockManagerConfig: bmConfig,
+			Light:              isLight,
+		},
+		keys[n],
+		signingKey,
+		proxy.NewLocalClientCreator(app),
+		genesis,
+		log.TestingLogger().With("node", n))
+	require.NoError(err)
+	require.NotNil(node)
+
+	return node, app
+}
+
 func getGenesisValidatorSetWithSigner(n int) ([]tmtypes.GenesisValidator, crypto.PrivKey) {
+	nodeKey := &p2p.NodeKey{
+		PrivKey: genesisValidatorKey,
+	}
+	signingKey, _ := conv.GetNodeKey(nodeKey)
+	pubKey := genesisValidatorKey.PubKey()
+
+	genesisValidators := []tmtypes.GenesisValidator{
+		{Address: pubKey.Address(), PubKey: pubKey, Power: int64(100), Name: "gen #1"},
+	}
+	return genesisValidators, signingKey
+}
+
+/*func getGenesisValidatorSetWithSigner(n int) ([]tmtypes.GenesisValidator, crypto.PrivKey) {
 	nodeKey := &p2p.NodeKey{
 		PrivKey: genesisValidatorKey,
 	}
@@ -135,3 +214,4 @@ func TestTrustMinimizedQuery(t *testing.T) {
 	fmt.Println(q)
 	cancel()
 }
+*/
