f13c2bc5a9b4b586714458e3f408d8991dd8f692
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2023-06-08 08:39:21 -0500

    minor golangci-lint fix

--- da/celestia/celestia_test.go
+++ da/celestia/celestia_test.go
@@ -4,8 +4,9 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/rollkit/rollkit/da"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/rollkit/rollkit/da"
 )
 
 func TestDataRequestErrorToStatus(t *testing.T) {
