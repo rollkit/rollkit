fcea1d992222934892b926a1d103815dc7165857
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-06-21 14:39:44 -0400

    add header verification

--- types/signed_header.go
+++ types/signed_header.go
@@ -1,9 +1,12 @@
 package types
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 
 	"github.com/celestiaorg/go-header"
+	abciconv "github.com/rollkit/rollkit/conv/abci"
 )
 
 func (sH *SignedHeader) New() header.Header {
@@ -22,7 +25,26 @@ func (sH *SignedHeader) Verify(untrst header.Header) error {
 		// and is a programmer bug
 		panic(fmt.Errorf("%T is not of type %T", untrst, untrstH))
 	}
+	err := untrstH.ValidateBasic()
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(untrstH.LastHeaderHash[:], sH.Header.Hash()) {
+		return errors.New("Last header hash does not match hash of previous header")
+	}
+	if !bytes.Equal(untrstH.LastCommitHash[:], sH.getLastCommitHash()) {
+		return errors.New("Last header hash does not match hash of previous header")
+	}
 	return sH.Header.Verify(&untrstH.Header)
 }
 
 var _ header.Header = &SignedHeader{}
+
+func (sH *SignedHeader) getLastCommitHash() []byte {
+	lastABCICommit := abciconv.ToABCICommit(sH.Commit, sH.BaseHeader.Height, sH.Hash())
+	if len(sH.Commit.Signatures) == 1 {
+		lastABCICommit.Signatures[0].ValidatorAddress = sH.ProposerAddress
+		lastABCICommit.Signatures[0].Timestamp = sH.Time()
+	}
+	return lastABCICommit.Hash()
+}
