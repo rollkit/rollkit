3499e5ec2f4b59b9e56ce93ed8e7586fdb36a441
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-06-27 00:16:32 -0400

    add test

---
+++ types/signed_header_test.go
@@ -0,0 +1,99 @@
+package types
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVerify(t *testing.T) {
+	h := NewTestSuite(t, 2).GenExtendedHeaders(3)
+	trusted, untrustedAdj, untrustedNonAdj := h[0], h[1], h[2]
+	tests := []struct {
+		prepare func() libhead.Header
+		err     bool
+	}{
+		{
+			prepare: func() libhead.Header { return untrustedAdj },
+			err:     false,
+		},
+		{
+			prepare: func() libhead.Header {
+				return untrustedNonAdj
+			},
+			err: false,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrusted := *untrustedAdj
+				untrusted.ValidatorsHash = tmrand.Bytes(32)
+				return &untrusted
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrusted := *untrustedNonAdj
+				untrusted.Commit = NewTestSuite(t, 2).Commit(RandRawHeader(t))
+				return &untrusted
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrusted := *untrustedAdj
+				untrusted.RawHeader.LastBlockID.Hash = tmrand.Bytes(32)
+				return &untrusted
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrustedAdj.RawHeader.Time = untrustedAdj.RawHeader.Time.Add(time.Minute)
+				return untrustedAdj
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrustedAdj.RawHeader.Time = untrustedAdj.RawHeader.Time.Truncate(time.Hour)
+				return untrustedAdj
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrustedAdj.RawHeader.ChainID = "toaster"
+				return untrustedAdj
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrustedAdj.RawHeader.Height++
+				return untrustedAdj
+			},
+			err: true,
+		},
+		{
+			prepare: func() libhead.Header {
+				untrustedAdj.RawHeader.Version.App = appconsts.LatestVersion + 1
+				return untrustedAdj
+			},
+			err: true,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			err := trusted.Verify(test.prepare())
+			if test.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
