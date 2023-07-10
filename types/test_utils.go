b6eb3261cccc7635dabab3d029aaf46061dc4f6a
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2023-07-10 07:18:24 -0500

    disable golangci-lint check for rand read

--- types/test_utils.go
+++ types/test_utils.go
@@ -100,7 +100,7 @@ func GetRandomTx() Tx {
 
 func GetRandomBytes(n int) []byte {
 	data := make([]byte, n)
-	_, _ = rand.Read(data) //nolint:gosec
+	_, _ = rand.Read(data) //nolint:gosec,staticcheck
 	return data
 }
 
