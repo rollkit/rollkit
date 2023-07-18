31d2601b00deb947ce0a62838d04d0440d9e54cb
Matthew Sevey <mjsevey@gmail.com>
2023-07-18 09:43:05 -0400

    Update log/test/loggers.go
    Co-authored-by: Tomasz Zdybał <tomek@zdybal.lap.pl>

--- log/test/loggers.go
+++ log/test/loggers.go
@@ -35,7 +35,7 @@ func NewFileLoggerCustom(t *testing.T, fileName string) *FileLogger {
 	if err != nil {
 		panic(err)
 	}
-	t.Log("Test logs can be found: logFile.Name()")
+	t.Log("Test logs can be found:", logFile.Name())
 	t.Cleanup(func() {
 		_ = logFile.Close()
 	})
