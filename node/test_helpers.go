839f2c931f2b75edc97fab90bfca311571c8bcb5
Matthew Sevey <mjsevey@gmail.com>
2023-07-24 10:22:09 -0400

    remove test code for debugging

--- node/test_helpers.go
+++ node/test_helpers.go
@@ -93,22 +93,7 @@ func waitForAtLeastNBlocks(node Node, n int, useBlockExchange bool) error {
 }
 
 // TODO: use n and return n validators
-func getTMGenesisValidatorSetWithSigner(n int) ([]cmtypes.GenesisValidator, crypto.PrivKey) {
-	nodeKey := &p2p.NodeKey{
-		PrivKey: genesisValidatorKey,
-	}
-	signingKey, _ := conv.GetNodeKey(nodeKey)
-	pubKey := genesisValidatorKey.PubKey()
-
-	genesisValidators := []cmtypes.GenesisValidator{
-		{Address: pubKey.Address(), PubKey: pubKey, Power: int64(100), Name: "gen #1"},
-	}
-	return genesisValidators, signingKey
-}
-
-// TODO: use n and return n validators
 func getGenesisValidatorSetWithSigner(n int) ([]cmtypes.GenesisValidator, crypto.PrivKey) {
-	getTMGenesisValidatorSetWithSigner(9)
 	nodeKey := &p2p.NodeKey{
 		PrivKey: genesisValidatorKey,
 	}
