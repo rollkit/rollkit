66cccc1496f35937829a3650f66bc6b65472ec24
chandiniv1 <velamalachandini9@gmail.com>
2023-09-07 16:29:28 +0530

    added sustrate api to dalc

--- da/avail/avail.go
+++ da/avail/avail.go
@@ -35,11 +35,12 @@ type Config struct {
 	Confidence float64 `json:"confidence"`
 }
 
-// DataAvailabilityLayerClient uses Avail-Node configuration parameters
+// DataAvailabilityLayerClient uses go-substrate-rpc-client
 type DataAvailabilityLayerClient struct {
 	_      types.NamespaceID
 	config Config
 	logger log.Logger
+	api    *gsrpc.SubstrateAPI
 }
 
 // Confidence stores block params retireved from Avail Light Node Endpoint
@@ -61,33 +62,31 @@ var _ da.BlockRetriever = &DataAvailabilityLayerClient{}
 // Init initializes DataAvailabilityLayerClient instance.
 func (c *DataAvailabilityLayerClient) Init(_ types.NamespaceID, config []byte, kvStore ds.Datastore, logger log.Logger) error {
 	c.logger = logger
-
 	if len(config) > 0 {
 		return json.Unmarshal(config, &c.config)
 	}
-
 	return nil
 }
 
 // Start prepares DataAvailabilityLayerClient to work.
 func (c *DataAvailabilityLayerClient) Start() error {
-
 	c.logger.Info("starting avail data availability layer client", "baseURL", c.config.ApiURL)
-
+	api, err := gsrpc.NewSubstrateAPI(c.config.ApiURL)
+	if err != nil {
+		return err
+	}
+	c.api = api
 	return nil
 }
 
 // Stop stops DataAvailabilityLayerClient.
 func (c *DataAvailabilityLayerClient) Stop() error {
-
 	c.logger.Info("stopping avail data availability layer client")
-
 	return nil
 }
 
 // SubmitBlock submits a block to DA layer.
 func (c *DataAvailabilityLayerClient) SubmitBlocks(ctx context.Context, blocks []*types.Block) da.ResultSubmitBlocks {
-
 	for _, block := range blocks {
 		data, err := block.MarshalBinary()
 		if err != nil {
@@ -98,7 +97,7 @@ func (c *DataAvailabilityLayerClient) SubmitBlocks(ctx context.Context, blocks [
 				},
 			}
 		}
-		if err := SubmitData(c.config.ApiURL, c.config.Seed, c.config.AppID, data); err != nil {
+		if err := c.submitData(data); err != nil {
 			return da.ResultSubmitBlocks{
 				BaseResult: da.BaseResult{
 					Code:    da.StatusError,
@@ -106,9 +105,7 @@ func (c *DataAvailabilityLayerClient) SubmitBlocks(ctx context.Context, blocks [
 				},
 			}
 		}
-
 	}
-
 	return da.ResultSubmitBlocks{
 		BaseResult: da.BaseResult{
 			Code:     da.StatusSuccess,
@@ -116,19 +113,14 @@ func (c *DataAvailabilityLayerClient) SubmitBlocks(ctx context.Context, blocks [
 			DAHeight: 1,
 		},
 	}
-
 }
 
 // RetrieveBlocks gets the block from DA layer.
 func (c *DataAvailabilityLayerClient) RetrieveBlocks(ctx context.Context, dataLayerHeight uint64) da.ResultRetrieveBlocks {
-
 	blocks := []*types.Block{}
-
 Loop:
 	blockNumber := dataLayerHeight
-
 	appDataURL := fmt.Sprintf(c.config.BaseURL+c.config.AppDataURL, blockNumber)
-
 	// Sanitize and validate the URL
 	parsedURL, err := url.Parse(appDataURL)
 	if err != nil {
@@ -139,7 +131,6 @@ Loop:
 			},
 		}
 	}
-
 	// Create an HTTP request with the sanitized URL
 	req, err := http.NewRequest("GET", parsedURL.String(), nil)
 	if err != nil {
@@ -150,7 +141,6 @@ Loop:
 			},
 		}
 	}
-
 	// Perform the HTTP request
 	client := http.DefaultClient
 	response, err := client.Do(req)
@@ -175,16 +165,11 @@ Loop:
 			},
 		}
 	}
-
 	var appDataObject AppData
 	if string(responseData) == BLOCK_NOT_FOUND {
-
 		appDataObject = AppData{Block: uint32(blockNumber), Extrinsics: []string{}}
-
 	} else if string(responseData) == PROCESSING_BLOCK {
-
 		goto Loop
-
 	} else {
 		if err = json.Unmarshal(responseData, &appDataObject); err != nil {
 			return da.ResultRetrieveBlocks{
@@ -195,12 +180,10 @@ Loop:
 			}
 		}
 	}
-
 	txnsByteArray := []byte{}
 	for _, extrinsic := range appDataObject.Extrinsics {
 		txnsByteArray = append(txnsByteArray, []byte(extrinsic)...)
 	}
-
 	block := &types.Block{
 		SignedHeader: types.SignedHeader{
 			Header: types.Header{
@@ -212,9 +195,7 @@ Loop:
 			Txs: types.Txs{txnsByteArray},
 		},
 	}
-
 	blocks = append(blocks, block)
-
 	return da.ResultRetrieveBlocks{
 		BaseResult: da.BaseResult{
 			Code:     da.StatusSuccess,
@@ -224,53 +205,54 @@ Loop:
 	}
 }
 
-func SubmitData(apiURL string, seed string, appID int, data []byte) error {
-
+// SubmitData submits data to the avail da node
+func (c *DataAvailabilityLayerClient) submitData(data []byte) error {
 	// if app id is greater than 0 then it must be created before submitting data
-	if appID < 1 {
+	if c.config.AppID < 1 {
 		return errors.New("AppID cant be 0")
 	}
 
-	api, err := gsrpc.NewSubstrateAPI(apiURL)
+	// fetching latest metadata
+	meta, err := c.api.RPC.State.GetMetadataLatest()
 	if err != nil {
 		return err
 	}
 
-	meta, err := api.RPC.State.GetMetadataLatest()
-	if err != nil {
-		return err
-	}
-
-	c, err := v4types.NewCall(meta, "DataAvailability.submit_data", data)
+	call, err := v4types.NewCall(meta, "DataAvailability.submit_data", data)
 	if err != nil {
 		return err
 	}
 
 	// Create the extrinsic
-	ext := v4types.NewExtrinsic(c)
+	ext := v4types.NewExtrinsic(call)
 
-	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
+	// get genesis block hash
+	genesisHash, err := c.api.RPC.Chain.GetBlockHash(0)
 	if err != nil {
 		return err
 	}
 
-	rv, err := api.RPC.State.GetRuntimeVersionLatest()
+	// get latest runtime version
+	rv, err := c.api.RPC.State.GetRuntimeVersionLatest()
 	if err != nil {
 		return err
 	}
 
-	keyringPair, err := signature.KeyringPairFromSecret(seed, 42)
+	// get the keyring pair
+	keyringPair, err := signature.KeyringPairFromSecret(c.config.Seed, 42)
 	if err != nil {
 		return err
 	}
 
+	// create storage key
 	key, err := v4types.CreateStorageKey(meta, "System", "Account", keyringPair.PublicKey)
 	if err != nil {
 		return err
 	}
 
+	// get latest storage info
 	var accountInfo v4types.AccountInfo
-	ok, err := api.RPC.State.GetStorageLatest(key, &accountInfo)
+	ok, err := c.api.RPC.State.GetStorageLatest(key, &accountInfo)
 	if err != nil || !ok {
 		return errors.New("failed to get the latest storage")
 	}
@@ -283,7 +265,7 @@ func SubmitData(apiURL string, seed string, appID int, data []byte) error {
 		Nonce:              v4types.NewUCompactFromUInt(uint64(nonce)),
 		SpecVersion:        rv.SpecVersion,
 		Tip:                v4types.NewUCompactFromUInt(0),
-		AppID:              v4types.NewUCompactFromUInt(uint64(appID)),
+		AppID:              v4types.NewUCompactFromUInt(uint64(c.config.AppID)),
 		TransactionVersion: rv.TransactionVersion,
 	}
 
@@ -294,7 +276,7 @@ func SubmitData(apiURL string, seed string, appID int, data []byte) error {
 	}
 
 	// Send the extrinsic
-	_, err = api.RPC.Author.SubmitExtrinsic(ext)
+	_, err = c.api.RPC.Author.SubmitExtrinsic(ext)
 	if err != nil {
 		return err
 	}
