af939ef3a88d507a8f19ca70a93b68f4cc35b1fc
Connor O'Hara <connor@switchboard.xyz>
2023-09-06 22:41:02 -0400

    validate header, to prevent junk blocks

--- block/manager.go
+++ block/manager.go
@@ -488,6 +488,12 @@ func (m *Manager) processNextDABlock(ctx context.Context) error {
 			}
 			m.logger.Debug("retrieved potential blocks", "n", len(blockResp.Blocks), "daHeight", daHeight)
 			for _, block := range blockResp.Blocks {
+				err := block.SignedHeader.ValidateBasic()
+				if err != nil {
+					// Discard junk blocks from random proposers
+					// but defer validating execution until later
+					continue
+				}
 				blockHash := block.Hash().String()
 				m.blockCache.setHardConfirmed(blockHash)
 				m.logger.Info("block marked as hard confirmed", "blockHeight", block.Height(), "blockHash", blockHash)
