419da5c1b8db2c40178b7288d3101dee0e2cc4f8
Connor O'Hara <connor@switchboard.xyz>
2023-10-01 22:56:52 -0400

    save

--- da/test/mock_test.go
+++ da/test/mock_test.go
@@ -1,8 +1,12 @@
-package mock
+package test
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/rollkit/rollkit/da/mock"
 	testlog "github.com/rollkit/rollkit/log/test"
 	"github.com/rollkit/rollkit/store"
 	"github.com/stretchr/testify/require"
@@ -14,7 +18,28 @@ func TestMockDA(t *testing.T) {
 	store, err := store.NewDefaultInMemoryKVStore()
 	require.NoError(err)
 
-	dalc := DataAvailabilityLayerClient{}
+	dalc := mock.DataAvailabilityLayerClient{}
 	err = dalc.Init([8]byte{0, 0, 0, 0, 0, 0, 0, 0}, []byte("1s"), store, testlog.NewLogger(t))
 	require.NoError(err)
+
+	err = dalc.Start()
+	require.NoError(err)
+	
+	blocks := getRandomBlock(0, 10)
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	
+	go func () {
+		for {
+			select {
+				<-cancel:
+
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 3)
+	err = dalc.Stop()
+	require.NoError(err)
+
 }
