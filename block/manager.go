f50a5369f8d4d2ddc74905727cc3fab40af89d84
Connor O'Hara <connor@switchboard.xyz>
2023-10-31 11:49:15 +0100

    comment out customValidate

--- block/manager.go
+++ block/manager.go
@@ -748,12 +748,13 @@ func (m *Manager) applyBlock(ctx context.Context, block *types.Block) (types.Sta
 	return m.executor.ApplyBlock(ctx, m.lastState, block)
 }
 
-func (m *Manager) customValidate(h types.SignedHeader) error {
+// Commenting this out until https://github.com/celestiaorg/go-header/pull/128 merged
+/*func (m *Manager) customValidate(h types.SignedHeader) error {
 	if bytes.Equal(h.ProposerAddress, m.lastState.Sequencer.Address().Bytes()) {
 		return nil
 	}
 	return fmt.Errorf("block proposer != centralized sequencer")
-}
+}*/
 
 func updateState(s *types.State, res *abci.ResponseInitChain) {
 	// If the app did not return an app hash, we keep the one set from the genesis doc in
