c68ff38b99656e586d7d7bc77f70b33b84ca04b0
Javed Khan <tuxcanfly@gmail.com>
2023-11-07 11:25:56 -0800

    test: update genesis validators helper

--- node/test_helpers.go
+++ node/test_helpers.go
@@ -117,21 +117,18 @@ func waitForAtLeastNBlocks(node Node, n int, source Source) error {
 	})
 }
 
-func getGenesisValidatorSetWithSigner(n int) ([]cmtypes.GenesisValidator, crypto.PrivKey) {
+func getGenesisValidatorSetWithSigner() ([]cmtypes.GenesisValidator, crypto.PrivKey) {
 	nodeKey := &p2p.NodeKey{
 		PrivKey: genesisValidatorKey,
 	}
 	signingKey, _ := GetNodeKey(nodeKey)
 	pubKey := genesisValidatorKey.PubKey()
 
-	genesisValidators := make([]cmtypes.GenesisValidator, n)
-	for i := 0; i < n; i++ {
-		genesisValidators[i] = cmtypes.GenesisValidator{
-			Address: pubKey.Address(),
-			PubKey:  pubKey,
-			Power:   int64(100),
-			Name:    fmt.Sprintf("gen #%d", i+1),
-		}
-	}
+	genesisValidators := []cmtypes.GenesisValidator{{
+		Address: pubKey.Address(),
+		PubKey:  pubKey,
+		Power:   int64(100),
+		Name:    fmt.Sprintf("gen #1"),
+	}}
 	return genesisValidators, signingKey
 }
