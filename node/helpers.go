1bd7f87764ba35b192093019e06565768f4282d9
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-11-15 12:06:29 +0300

    Separate out test helpers and helpers test

--- node/helpers.go
+++ node/helpers.go
@@ -1,8 +1,6 @@
 package node
 
 import (
-	"context"
-	"crypto/rand"
 	"errors"
 	"fmt"
 	"testing"
@@ -10,15 +8,11 @@ import (
 
 	testutils "github.com/celestiaorg/utils/test"
 	"github.com/cometbft/cometbft/crypto/ed25519"
-	"github.com/cometbft/cometbft/libs/log"
 	p2p "github.com/cometbft/cometbft/p2p"
 	cmtypes "github.com/cometbft/cometbft/types"
 	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/stretchr/testify/require"
 
 	"github.com/rollkit/rollkit/config"
-	mockda "github.com/rollkit/rollkit/da/mock"
-	"github.com/rollkit/rollkit/store"
 	"github.com/rollkit/rollkit/types"
 )
 
@@ -146,85 +140,4 @@ func getGenesisValidatorSetWithSigner() ([]cmtypes.GenesisValidator, crypto.Priv
 		Name:    "gen #1",
 	}}
 	return genesisValidators, signingKey
-}
-
-func TestMockTester(t *testing.T) {
-	m := MockTester{t}
-	m.Fail()
-	m.FailNow()
-	m.Logf("hello")
-	m.Errorf("goodbye")
-}
-
-func TestGetNodeHeight(t *testing.T) {
-	require := require.New(t)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	dalc := &mockda.DataAvailabilityLayerClient{}
-	ds, _ := store.NewDefaultInMemoryKVStore()
-	_ = dalc.Init([8]byte{}, nil, ds, log.TestingLogger())
-	_ = dalc.Start()
-	defer func() {
-		require.NoError(dalc.Stop())
-	}()
-	num := 2
-	keys := make([]crypto.PrivKey, num)
-	for i := 0; i < num; i++ {
-		keys[i], _, _ = crypto.GenerateEd25519Key(rand.Reader)
-	}
-	bmConfig := getBMConfig()
-	fullNode, _ := createNode(ctx, 0, true, false, keys, bmConfig, t)
-	lightNode, _ := createNode(ctx, 1, true, true, keys, bmConfig, t)
-	fullNode.(*FullNode).dalc = dalc
-	fullNode.(*FullNode).blockManager.SetDALC(dalc)
-	require.NoError(fullNode.Start())
-	defer func() {
-		require.NoError(fullNode.Stop())
-	}()
-
-	require.NoError(lightNode.Start())
-	defer func() {
-		require.NoError(lightNode.Stop())
-	}()
-
-	require.NoError(testutils.Retry(1000, 100*time.Millisecond, func() error {
-		num, err := getNodeHeight(fullNode, Header)
-		if err != nil {
-			return err
-		}
-		if num > 0 {
-			return nil
-		}
-		return errors.New("expected height > 0")
-	}))
-	require.NoError(testutils.Retry(1000, 100*time.Millisecond, func() error {
-		num, err := getNodeHeight(fullNode, Block)
-		if err != nil {
-			return err
-		}
-		if num > 0 {
-			return nil
-		}
-		return errors.New("expected height > 0")
-	}))
-	require.NoError(testutils.Retry(1000, 100*time.Millisecond, func() error {
-		num, err := getNodeHeight(fullNode, Store)
-		if err != nil {
-			return err
-		}
-		if num > 0 {
-			return nil
-		}
-		return errors.New("expected height > 0")
-	}))
-	require.NoError(testutils.Retry(1000, 100*time.Millisecond, func() error {
-		num, err := getNodeHeight(lightNode, Header)
-		if err != nil {
-			return err
-		}
-		if num > 0 {
-			return nil
-		}
-		return errors.New("expected height > 0")
-	}))
 }
