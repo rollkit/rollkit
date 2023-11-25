4789b82ed5630fc960d7769578dc9393981e61b5
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-11-24 19:40:26 -0500

    Update to return instead of if

--- node/full.go
+++ node/full.go
@@ -341,12 +341,11 @@ func (n *FullNode) OnStart() error {
 		go n.blockManager.BlockSubmissionLoop(n.ctx)
 		go n.headerPublishLoop(n.ctx)
 		go n.blockPublishLoop(n.ctx)
-	} else {
-		go n.blockManager.RetrieveLoop(n.ctx)
-		go n.blockManager.BlockStoreRetrieveLoop(n.ctx)
-		go n.blockManager.SyncLoop(n.ctx, n.cancel)
+		return nil
 	}
-
+	go n.blockManager.RetrieveLoop(n.ctx)
+	go n.blockManager.BlockStoreRetrieveLoop(n.ctx)
+	go n.blockManager.SyncLoop(n.ctx, n.cancel)
 	return nil
 }
 
