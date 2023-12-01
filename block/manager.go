9ac2778361b4b865b32e948bef1cf0d253a0a4f2
Connor O'Hara <connor@switchboard.xyz>
2023-12-01 14:08:37 -0500

    gofmt

--- block/manager.go
+++ block/manager.go
@@ -685,7 +685,7 @@ func (m *Manager) publishBlock(ctx context.Context) error {
 	block.SignedHeader.Validators = vals
 	err = m.store.SaveBlock(block, commit)
 	if err != nil {
-	    return err
+		return err
 	}
 	newState, responses, err := m.applyBlock(ctx, block)
 	if err != nil {
