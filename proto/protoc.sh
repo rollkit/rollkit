9a3bbbea51d702dcbcc9a947c8a657d732d91658
Javed Khan <tuxcanfly@gmail.com>
2023-11-30 22:04:21 -0800

    specs: add da spec (#1308)
    ## Overview
    
    This package adds specifications for the DA interface and celestia
    implementation.
    
    It also includes the failure modes when submitting rollup blocks to the
    da layer.
    
    ## Checklist
    
    - [x] New and updated code has appropriate documentation
    - [x] New and updated code has new and/or updated testing
    - [x] Required CI checks are passing
    - [x] Visual proof for any user facing features like CLI or
    documentation updates
    - [x] Linked issues closed with keywords
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a Data Availability Layer Client (DALC) interface for
    improved interaction with Data Availability layers.
    - Implemented methods for submitting and retrieving blocks within the
    DALC.
    - Added gRPC service communication with protobuf serialization for
    rollup blocks.
    
    - **Documentation**
    - Provided detailed descriptions and assumptions for interacting with
    the Data Availability layer.
    - Outlined failure cases and retry mechanisms for robust error handling.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -2,6 +2,5 @@
 
 set -eo pipefail
 
-buf generate --path="./proto/dalc" --template="buf.gen.yaml" --config="buf.yaml"
 buf generate --path="./proto/rollkit" --template="buf.gen.yaml" --config="buf.yaml"
 buf generate --path="./proto/tendermint/abci" --template="buf.gen.yaml" --config="buf.yaml"
