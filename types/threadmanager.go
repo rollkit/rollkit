88ec151e5a987c3ee36577f7785b2fe3900f28e4
Matthew Sevey <mjsevey@gmail.com>
2024-01-07 12:06:43 -0500

    chore: add comments to threadmanager to resolve lint issues

--- types/threadmanager.go
+++ types/threadmanager.go
@@ -8,14 +8,17 @@ type ThreadManager struct {
 	wg sync.WaitGroup
 }
 
+// NewThreadManager creates a new ThreadManager
 func NewThreadManager() *ThreadManager {
 	return &ThreadManager{}
 }
 
+// Wait blocks until all goroutines have called Done on the WaitGroup
 func (tm *ThreadManager) Wait() {
 	tm.wg.Wait()
 }
 
+// Go launches a goroutine and adds it to the WaitGroup
 func (tm *ThreadManager) Go(f func()) {
 	tm.wg.Add(1)
 	go func() {
