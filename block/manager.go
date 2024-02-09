6044dab680f8846989fa0dd9d5f2134d01dd519d
Javed Khan <tuxcanfly@gmail.com>
2024-02-08 17:01:31 -0800

    fix(block/manager): limit consensus_params.block.max_bytes to da maxblobsize

--- block/manager.go
+++ block/manager.go
@@ -48,6 +48,9 @@ const channelLength = 100
 // Applies to the blockInCh, 10000 is a large enough number for blocks per DA block.
 const blockInChLength = 10000
 
+// blockProtocolOverhead is the overhead in bytes of serializing the block into blob.
+const blockProtocolOverhead = 100
+
 // initialBackoff defines initial value for block submission backoff
 var initialBackoff = 100 * time.Millisecond
 
@@ -170,6 +173,16 @@ func NewManager(
 	if conf.DAMempoolTTL == 0 {
 		logger.Info("Using default mempool ttl", "MempoolTTL", defaultMempoolTTL)
 		conf.DAMempoolTTL = defaultMempoolTTL
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	maxBlobSize, err := dalc.DA.MaxBlobSize(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if genesis.ConsensusParams.Block.MaxBytes >= int64(maxBlobSize) {
+		genesis.ConsensusParams.Block.MaxBytes = int64(maxBlobSize) - blockProtocolOverhead
 	}
 
 	proposerAddress, err := getAddress(proposerKey)
