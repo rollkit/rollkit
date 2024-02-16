b4c30f815978225d7fe358bd211dcbaef990e9e3
Matthew Sevey <mjsevey@gmail.com>
2024-02-16 10:32:18 -0500

    feat: update the docs-gen cli command to clear out the docs folder of old files

--- cmd/rollkit/commands/docs_gen.go
+++ cmd/rollkit/commands/docs_gen.go
@@ -1,15 +1,29 @@
 package commands
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
+
+var docsDirectory = "./cmd/rollkit/docs"
 
 // DocsGenCmd is the command to generate documentation for rollkit CLI
 var DocsGenCmd = &cobra.Command{
 	Use:   "docs-gen",
 	Short: "Generate documentation for rollkit CLI",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return doc.GenMarkdownTree(RootCmd, "./cmd/rollkit/docs")
+		// Clear out the docs directory
+		err := os.RemoveAll(docsDirectory)
+		if err != nil {
+			return err
+		}
+		// Initiate the docs directory
+		err = os.MkdirAll(docsDirectory, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		return doc.GenMarkdownTree(RootCmd, docsDirectory)
 	},
 }
