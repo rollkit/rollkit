8ec09c1d901d9a7789f850bec6bc0dd5860dee63
Tomasz Zdybał <tomek@zdybal.lap.pl>
2024-02-27 15:06:39 +0100

    test: Add test for pending blocks and refactor code
    TestPendingBlocks should fail to proof the existence of the bug.
    
    This commit introduces a new test to check pending blocks as described in issue #1548. It mimics the behavior of a node producing blocks, stopping and restarting, then producing more blocks.

--- node/full_node_test.go
+++ node/full_node_test.go
@@ -3,9 +3,18 @@ package node
 import (
 	"context"
 	"crypto/rand"
+	"crypto/sha256"
+	"errors"
 	"fmt"
+	"os"
 	"testing"
 	"time"
+
+	cmconfig "github.com/cometbft/cometbft/config"
+	"github.com/cometbft/cometbft/proxy"
+	goDA "github.com/rollkit/go-da"
+	"github.com/rollkit/rollkit/config"
+	test "github.com/rollkit/rollkit/test/log"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -147,6 +156,113 @@ func TestInvalidBlocksIgnored(t *testing.T) {
 	require.NoError(t, waitUntilBlockHashSeen(node, b1.Hash().String()))
 	require.True(t, b1.Hash().String() == junkCommitBlock.Hash().String())
 	require.False(t, manager.IsBlockHashSeen(junkProposerBlock.Hash().String()))
+}
+
+// TestPendingBlocks is a test for bug described in https://github.com/rollkit/rollkit/issues/1548
+func TestPendingBlocks(t *testing.T) {
+	ctx := context.Background()
+
+	mockDA := new(mocks.DA)
+	mockDA.On("MaxBlobSize", mock.Anything).Return(uint64(10240), nil)
+	mockDA.On("Submit", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, errors.New("DA not available"))
+
+	dac := &da.DAClient{
+		DA:        mockDA,
+		Namespace: goDA.Namespace(MockNamespace),
+		GasPrice:  1234,
+	}
+	dbPath, err := os.MkdirTemp("", "testdb")
+	require.NoError(t, err)
+	defer func() {
+		_ = os.RemoveAll(dbPath)
+	}()
+
+	node, _ := createAggregatorWithPersistence(ctx, dbPath, dac, t)
+	err = node.Start()
+	assert.NoError(t, err)
+
+	const firstRunBlocks = 10
+
+	err = waitForAtLeastNBlocks(node, firstRunBlocks, Store)
+	assert.NoError(t, err)
+
+	err = node.Stop()
+	assert.NoError(t, err)
+
+	// create & start new node
+	node, _ = createAggregatorWithPersistence(ctx, dbPath, dac, t)
+
+	// reset DA mock to ensure that Submit was called
+	mockDA.On("Submit", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Unset()
+
+	// mock submit function to just return some hash and count the number of submitted blobs
+	// node will be stopped after producing at least firstRunBlocks blocks
+	// restarted node should get blocks from first and second run (more than firstRunBlocks
+	// TODO(tzdybal): this seems fragile and probably should be improved (to deserialize blocks and check heights)
+	uniqueBlobs := make(map[string]uint64)
+	mockDA.On("Submit", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, blobs [][]byte, gasPrice float64, namespace []byte) ([][]byte, error) {
+			hashes := make([][]byte, len(blobs))
+			for i, blob := range blobs {
+				sha := sha256.Sum256(blob)
+				hashes[i] = sha[:]
+				uniqueBlobs[string(sha[:])]++
+			}
+			return hashes, nil
+		})
+
+	err = node.Start()
+	assert.NoError(t, err)
+
+	// let node produce few more blocks
+	err = waitForAtLeastNBlocks(node, firstRunBlocks+5, Store)
+	assert.NoError(t, err)
+
+	err = node.Stop()
+	assert.NoError(t, err)
+
+	assert.Greater(t, len(uniqueBlobs), firstRunBlocks)
+	mock.AssertExpectationsForObjects(t, mockDA)
+}
+
+func createAggregatorWithPersistence(ctx context.Context, dbPath string, dalc *da.DAClient, t *testing.T) (Node, *mocks.Application) {
+	t.Helper()
+
+	key, _, _ := crypto.GenerateEd25519Key(rand.Reader)
+	genesis, genesisValidatorKey := types.GetGenesisWithPrivkey()
+	signingKey, err := types.PrivKeyToSigningKey(genesisValidatorKey)
+	require.NoError(t, err)
+
+	app := getMockApplication()
+
+	node, err := NewNode(
+		ctx,
+		config.NodeConfig{
+			DBPath:      dbPath,
+			DAAddress:   MockServerAddr,
+			DANamespace: MockNamespace,
+			Aggregator:  true,
+			BlockManagerConfig: config.BlockManagerConfig{
+				BlockTime:   100 * time.Millisecond,
+				DABlockTime: 300 * time.Millisecond,
+			},
+			Light: false,
+		},
+		key,
+		signingKey,
+		proxy.NewLocalClientCreator(app),
+		genesis,
+		DefaultMetricsProvider(cmconfig.DefaultInstrumentationConfig()),
+		test.NewFileLoggerCustom(t, test.TempLogFileName(t, "")),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, node)
+
+	fullNode := node.(*FullNode)
+	fullNode.dalc = dalc
+	fullNode.blockManager.SetDALC(dalc)
+
+	return fullNode, app
 }
 
 // setupMockApplication initializes a mock application
