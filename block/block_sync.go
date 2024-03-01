32b37d9c07c841171d8ac00cb5b7c0ca1ae4cf44
Javed Khan <tuxcanfly@gmail.com>
2024-03-01 07:56:55 -0800

    fix(sync): check height bound initial height

--- block/block_sync.go
+++ block/block_sync.go
@@ -99,8 +99,8 @@ func (bSyncService *BlockSyncService) initBlockStoreAndStartSyncer(ctx context.C
 // Note: Only returns an error in case block store can't be initialized. Logs
 // error if there's one while broadcasting.
 func (bSyncService *BlockSyncService) WriteToBlockStoreAndBroadcast(ctx context.Context, block *types.Block) error {
-	if bSyncService.genesis.InitialHeight < 0 || block.Height() < 0 {
-		return fmt.Errorf("invalid block height; cannot be negative")
+	if bSyncService.genesis.InitialHeight < 0 {
+		return fmt.Errorf("invalid initial height; cannot be negative")
 	}
 	isGenesis := block.Height() == uint64(bSyncService.genesis.InitialHeight)
 	// For genesis block initialize the store and start the syncer
