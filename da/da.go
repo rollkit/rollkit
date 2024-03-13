1b13b3114cb1eea287983470ae6c64cbc85764cf
Javed Khan <tuxcanfly@gmail.com>
2024-03-12 18:39:38 -0700

    fix(da): debug ctx timeout

--- da/da.go
+++ da/da.go
@@ -18,7 +18,7 @@ import (
 
 var (
 	// submitTimeout is the timeout for block submission
-	submitTimeout = 60 * time.Second
+	submitTimeout = 120 * time.Second
 
 	// retrieveTimeout is the timeout for block retrieval
 	retrieveTimeout = 60 * time.Second
