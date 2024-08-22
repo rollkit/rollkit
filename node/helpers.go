b502408f26f4185b12f99f6cbcba67cde0fb3095
gupadhyaya <ganeshrvce@gmail.com>
2024-08-22 14:14:43 +0400

    fix golangci-lint

--- node/helpers.go
+++ node/helpers.go
@@ -96,13 +96,13 @@ func getNodeHeightFromStore(node Node) (uint64, error) {
 }
 
 // safeClose closes the channel if it's not closed already
-func safeClose(ch chan struct{}) {
-	select {
-	case <-ch:
-	default:
-		close(ch)
-	}
-}
+// func safeClose(ch chan struct{}) {
+// 	select {
+// 	case <-ch:
+// 	default:
+// 		close(ch)
+// 	}
+// }
 
 func verifyNodesSynced(node1, node2 Node, source Source) error {
 	return testutils.Retry(300, 100*time.Millisecond, func() error {
