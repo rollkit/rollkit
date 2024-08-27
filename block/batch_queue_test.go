1eebbfd0c162d1a9d80371427343add706d3ae0c
gupadhyaya <ganeshrvce@gmail.com>
2024-08-27 17:46:50 +0400

    fix golangci-lint

--- block/batch_queue_test.go
+++ block/batch_queue_test.go
@@ -3,8 +3,9 @@ package block
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/rollkit/go-sequencing"
-	"github.com/stretchr/testify/assert"
 )
 
 // MockBatch is a mock implementation of sequencing.Batch for testing purposes
@@ -24,8 +25,8 @@ var (
 func TestNewBatchQueue(t *testing.T) {
 	// Test creating a new BatchQueue
 	bq := NewBatchQueue()
-	assert.NotNil(t, bq, "NewBatchQueue should return a non-nil instance")
-	assert.Empty(t, bq.queue, "New BatchQueue should have an empty queue")
+	require.NotNil(t, bq, "NewBatchQueue should return a non-nil instance")
+	require.Empty(t, bq.queue, "New BatchQueue should have an empty queue")
 }
 
 func TestBatchQueue_AddBatch(t *testing.T) {
@@ -34,13 +35,13 @@ func TestBatchQueue_AddBatch(t *testing.T) {
 
 	// Add the first batch and check
 	bq.AddBatch(batch1)
-	assert.Len(t, bq.queue, 1, "BatchQueue should have 1 batch after adding")
-	assert.Equal(t, batch1, bq.queue[0], "The first batch should match the one added")
+	require.Len(t, bq.queue, 1, "BatchQueue should have 1 batch after adding")
+	require.Equal(t, batch1, bq.queue[0], "The first batch should match the one added")
 
 	// Add the second batch and check
 	bq.AddBatch(batch2)
-	assert.Len(t, bq.queue, 2, "BatchQueue should have 2 batches after adding another")
-	assert.Equal(t, batch2, bq.queue[1], "The second batch should match the one added")
+	require.Len(t, bq.queue, 2, "BatchQueue should have 2 batches after adding another")
+	require.Equal(t, batch2, bq.queue[1], "The second batch should match the one added")
 }
 
 func TestBatchQueue_Next(t *testing.T) {
@@ -48,7 +49,7 @@ func TestBatchQueue_Next(t *testing.T) {
 	bq := NewBatchQueue()
 
 	// Test with empty queue
-	assert.Nil(t, bq.Next(), "Next should return nil when the queue is empty")
+	require.Nil(t, bq.Next(), "Next should return nil when the queue is empty")
 
 	// Add batches
 	bq.AddBatch(batch1)
@@ -56,13 +57,13 @@ func TestBatchQueue_Next(t *testing.T) {
 
 	// Retrieve the first batch
 	nextBatch := bq.Next()
-	assert.NotNil(t, nextBatch, "Next should return the first batch when called")
-	assert.Equal(t, batch1, *nextBatch, "Next should return the first batch added")
-	assert.Len(t, bq.queue, 1, "BatchQueue should have 1 batch after retrieving the first")
+	require.NotNil(t, nextBatch, "Next should return the first batch when called")
+	require.Equal(t, batch1, *nextBatch, "Next should return the first batch added")
+	require.Len(t, bq.queue, 1, "BatchQueue should have 1 batch after retrieving the first")
 
 	// Retrieve the second batch
 	nextBatch = bq.Next()
-	assert.NotNil(t, nextBatch, "Next should return the second batch when called")
-	assert.Equal(t, batch2, *nextBatch, "Next should return the second batch added")
-	assert.Empty(t, bq.queue, "BatchQueue should be empty after retrieving all batches")
+	require.NotNil(t, nextBatch, "Next should return the second batch when called")
+	require.Equal(t, batch2, *nextBatch, "Next should return the second batch added")
+	require.Empty(t, bq.queue, "BatchQueue should be empty after retrieving all batches")
 }
