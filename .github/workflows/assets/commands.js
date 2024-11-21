afe389bc27b22a81d9f48f463e13086d96da68d8
MSevey <15232757+MSevey@users.noreply.github.com>
2024-11-21 14:58:24 -0500

    fix(ci): update tutorial testing ci file

--- .github/workflows/assets/commands.js
+++ .github/workflows/assets/commands.js
@@ -1,4 +1,11 @@
-const commands = Object.freeze({
-    rollkitStart: "rollkit start --ci",
-  });
+const commands = Object.freeze([
+    {
+      name: "Another Command",
+      command: "echo 'This is another command!'",
+    },
+    {
+      name: "Rollkit Start",
+      command: "rollkit start --ci",
+    },
+  ]);
   export default commands;
