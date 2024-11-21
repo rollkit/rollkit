b2cc98bd2a3769e9b6593df9e02b9a0f6b581fd6
MSevey <15232757+MSevey@users.noreply.github.com>
2024-11-21 15:21:31 -0500

    add remaining commands

--- .github/workflows/assets/commands.js
+++ .github/workflows/assets/commands.js
@@ -1,11 +1,39 @@
 const commands = Object.freeze([
     {
-      name: "Another Command",
-      command: "echo 'This is another command!'",
+      name: "Install Ignite",
+      command: "curl https://get.ignite.com/cli@v28.5.3! | bash",
+    },
+    {
+      name: "Scaffold Chain",
+      command: "ignite scaffold chain gm --address-prefix gm --minimal --skip-proto",
+    },
+    {
+      name: "cd gm",
+      command: "cd gm",
+    },
+    {
+      name: "Install Rollkit App",
+      command: "ignite app install github.com/ignite/apps/rollkit@rollkit/v0.2.1",
+    },
+    {
+      name: "Add Rollkit App",
+      command: "ignite rollkit add",
+    },
+    {
+      name: "Build Chain",
+      command: "ignite chain build",
+    },
+    {
+      name: "Init Rollkit",
+      command: "ignite rollkit init",
+    },
+    {
+      name: "Init Rollkit toml",
+      command: "rollkit toml init",
     },
     {
       name: "Rollkit Start",
-      command: "rollkit start --ci",
+      command: "rollkit start --rollkit.aggregator --rollkit.sequencer_rollup_id gm --ci",
     },
   ]);
   export default commands;
