e6e616e71ded17253b384a62b019f71ee26a0b84
Facundo <facundomedica@gmail.com>
2025-02-25 16:19:11 +0100

    shorten db keys

--- block/manager.go
+++ block/manager.go
@@ -73,10 +73,10 @@ const headerInChLength = 10000
 var initialBackoff = 100 * time.Millisecond
 
 // DAIncludedHeightKey is the key used for persisting the da included height in store.
-const DAIncludedHeightKey = "da included height"
+const DAIncludedHeightKey = "d"
 
 // LastBatchHashKey is the key used for persisting the last batch hash in store.
-const LastBatchHashKey = "last batch hash"
+const LastBatchHashKey = "l"
 
 // dataHashForEmptyTxs to be used while only syncing headers from DA and no p2p to get the Data for no txs scenarios, the syncing can proceed without getting stuck forever.
 var dataHashForEmptyTxs = []byte{110, 52, 11, 156, 255, 179, 122, 152, 156, 165, 68, 230, 187, 120, 10, 44, 120, 144, 29, 63, 179, 55, 56, 118, 133, 17, 163, 6, 23, 175, 160, 29}
