382454fe1cca439a72e9886f43afa92036ef4c46
Randy Grok <98407738+randygrok@users.noreply.github.com>
2025-02-26 15:19:54 +0100

    feat: clean executor and indexer. (#2004)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    
    NOTE: PR titles should follow semantic commits:
    https://www.conventionalcommits.org/en/v1.0.0/
    -->
    
    ## Overview
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    
    Ex: Closes #<issue number>
    -->
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new configuration option for setting the executor
    middleware address (default is "localhost:40041").
    
    - **Refactoring & Improvements**
    - Streamlined node architecture by removing legacy RPC, client, and
    indexing components for simplified operations and enhanced performance.
      - Updated header hashing to use direct SHA-256 computation.
    - Adjusted genesis initialization so that block numbering now starts at
    1.
    
    - **Chores**
    - Removed obsolete tests and documentation, modernizing the codebase for
    improved maintainability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- node/full.go
+++ node/full.go
@@ -34,11 +34,6 @@ import (
 	"github.com/rollkit/rollkit/da"
 	"github.com/rollkit/rollkit/mempool"
 	"github.com/rollkit/rollkit/p2p"
-	"github.com/rollkit/rollkit/state"
-	"github.com/rollkit/rollkit/state/indexer"
-	blockidxkv "github.com/rollkit/rollkit/state/indexer/block/kv"
-	"github.com/rollkit/rollkit/state/txindex"
-	"github.com/rollkit/rollkit/state/txindex/kv"
 	"github.com/rollkit/rollkit/store"
 	"github.com/rollkit/rollkit/types"
 )
@@ -80,10 +75,7 @@ type FullNode struct {
 	blockManager *block.Manager
 
 	// Preserves cometBFT compatibility
-	TxIndexer      txindex.TxIndexer
-	BlockIndexer   indexer.BlockIndexer
-	IndexerService *txindex.IndexerService
-	prometheusSrv  *http.Server
+	prometheusSrv *http.Server
 
 	// keep context here only because of API compatibility
 	// - it's used in `OnStart` (defined in service.Service interface)
@@ -113,7 +105,7 @@ func newFullNode(
 		}
 	}()
 
-	seqMetrics, p2pMetrics, smMetrics := metricsProvider(genesis.ChainID)
+	seqMetrics, p2pMetrics := metricsProvider(genesis.ChainID)
 
 	eventBus, err := initEventBus(logger)
 	if err != nil {
@@ -150,34 +142,25 @@ func newFullNode(
 
 	store := store.New(mainKV)
 
-	blockManager, err := initBlockManager(signingKey, nodeConfig, genesis, store, seqClient, dalc, eventBus, logger, headerSyncService, dataSyncService, seqMetrics, smMetrics)
-	if err != nil {
-		return nil, err
-	}
-
-	indexerKV := newPrefixKV(baseKV, indexerPrefix)
-	indexerService, txIndexer, blockIndexer, err := createAndStartIndexerService(ctx, indexerKV, eventBus, logger)
+	blockManager, err := initBlockManager(signingKey, nodeConfig, genesis, store, seqClient, dalc, eventBus, logger, headerSyncService, dataSyncService, seqMetrics)
 	if err != nil {
 		return nil, err
 	}
 
 	node := &FullNode{
-		eventBus:       eventBus,
-		genesis:        genesis,
-		nodeConfig:     nodeConfig,
-		p2pClient:      p2pClient,
-		blockManager:   blockManager,
-		dalc:           dalc,
-		seqClient:      seqClient,
-		Store:          store,
-		TxIndexer:      txIndexer,
-		IndexerService: indexerService,
-		BlockIndexer:   blockIndexer,
-		hSyncService:   headerSyncService,
-		dSyncService:   dataSyncService,
-		ctx:            ctx,
-		cancel:         cancel,
-		threadManager:  types.NewThreadManager(),
+		eventBus:      eventBus,
+		genesis:       genesis,
+		nodeConfig:    nodeConfig,
+		p2pClient:     p2pClient,
+		blockManager:  blockManager,
+		dalc:          dalc,
+		seqClient:     seqClient,
+		Store:         store,
+		hSyncService:  headerSyncService,
+		dSyncService:  dataSyncService,
+		ctx:           ctx,
+		cancel:        cancel,
+		threadManager: types.NewThreadManager(),
 	}
 
 	node.BaseService = *service.NewBaseService(logger, "Node", node)
@@ -244,7 +227,7 @@ func initDataSyncService(mainKV ds.TxnDatastore, nodeConfig config.NodeConfig, g
 	return dataSyncService, nil
 }
 
-func initBlockManager(signingKey crypto.PrivKey, nodeConfig config.NodeConfig, genesis *cmtypes.GenesisDoc, store store.Store, seqClient *seqGRPC.Client, dalc *da.DAClient, eventBus *cmtypes.EventBus, logger log.Logger, headerSyncService *block.HeaderSyncService, dataSyncService *block.DataSyncService, seqMetrics *block.Metrics, execMetrics *state.Metrics) (*block.Manager, error) {
+func initBlockManager(signingKey crypto.PrivKey, nodeConfig config.NodeConfig, genesis *cmtypes.GenesisDoc, store store.Store, seqClient *seqGRPC.Client, dalc *da.DAClient, eventBus *cmtypes.EventBus, logger log.Logger, headerSyncService *block.HeaderSyncService, dataSyncService *block.DataSyncService, seqMetrics *block.Metrics) (*block.Manager, error) {
 	exec, err := initExecutor(nodeConfig)
 	if err != nil {
 		return nil, fmt.Errorf("error while initializing executor: %w", err)
@@ -258,7 +241,7 @@ func initBlockManager(signingKey crypto.PrivKey, nodeConfig config.NodeConfig, g
 		ChainID:         genesis.ChainID,
 		ProposerAddress: genesis.Validators[0].Address.Bytes(),
 	}
-	blockManager, err := block.NewManager(context.TODO(), signingKey, nodeConfig.BlockManagerConfig, rollGen, store, exec, seqClient, dalc, eventBus, logger.With("module", "BlockManager"), headerSyncService.Store(), dataSyncService.Store(), seqMetrics, execMetrics)
+	blockManager, err := block.NewManager(context.TODO(), signingKey, nodeConfig.BlockManagerConfig, rollGen, store, exec, seqClient, dalc, logger.With("module", "BlockManager"), headerSyncService.Store(), dataSyncService.Store(), seqMetrics)
 	if err != nil {
 		return nil, fmt.Errorf("error while initializing BlockManager: %w", err)
 	}
@@ -434,7 +417,6 @@ func (n *FullNode) OnStop() {
 		n.hSyncService.Stop(n.ctx),
 		n.dSyncService.Stop(n.ctx),
 		n.seqClient.Stop(),
-		n.IndexerService.Stop(),
 	)
 	if n.prometheusSrv != nil {
 		err = errors.Join(err, n.prometheusSrv.Shutdown(n.ctx))
@@ -508,30 +490,6 @@ func (n *FullNode) newTxValidator(metrics *p2p.Metrics) p2p.GossipValidator {
 
 func newPrefixKV(kvStore ds.Datastore, prefix string) ds.TxnDatastore {
 	return (ktds.Wrap(kvStore, ktds.PrefixTransform{Prefix: ds.NewKey(prefix)}).Children()[0]).(ds.TxnDatastore)
-}
-
-func createAndStartIndexerService(
-	ctx context.Context,
-	kvStore ds.TxnDatastore,
-	eventBus *cmtypes.EventBus,
-	logger log.Logger,
-) (*txindex.IndexerService, txindex.TxIndexer, indexer.BlockIndexer, error) {
-	var (
-		txIndexer    txindex.TxIndexer
-		blockIndexer indexer.BlockIndexer
-	)
-
-	txIndexer = kv.NewTxIndex(ctx, kvStore)
-	blockIndexer = blockidxkv.New(ctx, newPrefixKV(kvStore, "block_events"))
-
-	indexerService := txindex.NewIndexerService(ctx, txIndexer, blockIndexer, eventBus, false)
-	indexerService.SetLogger(logger.With("module", "txindex"))
-
-	if err := indexerService.Start(); err != nil {
-		return nil, nil, nil, err
-	}
-
-	return indexerService, txIndexer, blockIndexer, nil
 }
 
 // Start implements NodeLifecycle
--- node/setup.go
+++ node/setup.go
@@ -7,23 +7,21 @@ import (
 
 	"github.com/rollkit/rollkit/block"
 	"github.com/rollkit/rollkit/p2p"
-	"github.com/rollkit/rollkit/state"
 )
 
 const readHeaderTimeout = 10 * time.Second
 
 // MetricsProvider returns a consensus, p2p and mempool Metrics.
-type MetricsProvider func(chainID string) (*block.Metrics, *p2p.Metrics, *state.Metrics)
+type MetricsProvider func(chainID string) (*block.Metrics, *p2p.Metrics)
 
 // DefaultMetricsProvider returns Metrics build using Prometheus client library
 // if Prometheus is enabled. Otherwise, it returns no-op Metrics.
 func DefaultMetricsProvider(config *cmcfg.InstrumentationConfig) MetricsProvider {
-	return func(chainID string) (*block.Metrics, *p2p.Metrics, *state.Metrics) {
+	return func(chainID string) (*block.Metrics, *p2p.Metrics) {
 		if config.Prometheus {
 			return block.PrometheusMetrics(config.Namespace, "chain_id", chainID),
-				p2p.PrometheusMetrics(config.Namespace, "chain_id", chainID),
-				state.PrometheusMetrics(config.Namespace, "chain_id", chainID)
+				p2p.PrometheusMetrics(config.Namespace, "chain_id", chainID)
 		}
-		return block.NopMetrics(), p2p.NopMetrics(), state.NopMetrics()
+		return block.NopMetrics(), p2p.NopMetrics()
 	}
 }
