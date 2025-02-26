b66233bad634d0b43510d467553ba537c16101f6
tac0turtle <markobaricevic@MacBookPro.localdomain>
2025-02-26 13:59:47 +0100

    remove isr code

--- types/tx.go
+++ types/tx.go
@@ -1,17 +1,13 @@
 package types
 
 import (
-	"fmt"
-
 	"github.com/cometbft/cometbft/crypto/merkle"
 	"github.com/cometbft/cometbft/crypto/tmhash"
 	cmbytes "github.com/cometbft/cometbft/libs/bytes"
-	"google.golang.org/protobuf/proto"
 
 	"github.com/rollkit/rollkit/third_party/celestia-app/appconsts"
 	appns "github.com/rollkit/rollkit/third_party/celestia-app/namespace"
 	"github.com/rollkit/rollkit/third_party/celestia-app/shares"
-	pb "github.com/rollkit/rollkit/types/pb/rollkit"
 )
 
 // Tx represents transaction.
@@ -59,39 +55,6 @@ type TxProof struct {
 	Proof    merkle.Proof     `json:"proof"`
 }
 
-// ToTxsWithISRs converts a slice of transactions and a list of intermediate state roots
-// to a slice of TxWithISRs. Note that the length of intermediateStateRoots is
-// equal to the length of txs + 1.
-func (txs Txs) ToTxsWithISRs(intermediateStateRoots IntermediateStateRoots) ([]pb.TxWithISRs, error) {
-	expectedISRListLength := len(txs) + 1
-	if len(intermediateStateRoots.RawRootsList) != expectedISRListLength {
-		return nil, fmt.Errorf("invalid length of ISR list: %d, expected length: %d", len(intermediateStateRoots.RawRootsList), expectedISRListLength)
-	}
-	txsWithISRs := make([]pb.TxWithISRs, len(txs))
-	for i, tx := range txs {
-		txsWithISRs[i] = pb.TxWithISRs{}
-		txsWithISRs[i].SetPreIsr(intermediateStateRoots.RawRootsList[i])
-		txsWithISRs[i].SetTx(tx)
-		txsWithISRs[i].SetPostIsr(intermediateStateRoots.RawRootsList[i+1])
-	}
-	return txsWithISRs, nil
-}
-
-// TxsWithISRsToShares converts a slice of TxWithISRs to a slice of shares.
-func TxsWithISRsToShares(txsWithISRs []pb.TxWithISRs) (txShares []shares.Share, err error) {
-	byteSlices := make([][]byte, len(txsWithISRs))
-
-	for i := 0; i < len(txsWithISRs); i++ {
-		byteSlices[i], err = proto.Marshal(&txsWithISRs[i])
-		if err != nil {
-			return nil, err
-		}
-	}
-	coreTxs := shares.TxsFromBytes(byteSlices)
-	txShares, err = shares.SplitTxs(coreTxs)
-	return txShares, err
-}
-
 // SharesToPostableBytes converts a slice of shares to a slice of bytes that can
 // be posted to the blockchain.
 func SharesToPostableBytes(txShares []shares.Share) (postableData []byte, err error) {
@@ -115,22 +78,4 @@ func PostableBytesToShares(postableData []byte) (txShares []shares.Share, err er
 	}
 	shares, _, err := css.Export(0)
 	return shares, err
-}
-
-// SharesToTxsWithISRs converts a slice of shares to a slice of TxWithISRs.
-func SharesToTxsWithISRs(txShares []shares.Share) (txsWithISRs []*pb.TxWithISRs, err error) {
-	byteSlices, err := shares.ParseCompactShares(txShares)
-	if err != nil {
-		return nil, err
-	}
-	txsWithISRs = make([]*pb.TxWithISRs, len(byteSlices))
-	for i, byteSlice := range byteSlices {
-		txWithISR := new(pb.TxWithISRs)
-		err = proto.Unmarshal(byteSlice, txWithISR)
-		if err != nil {
-			return nil, err
-		}
-		txsWithISRs[i] = txWithISR
-	}
-	return txsWithISRs, nil
 }
