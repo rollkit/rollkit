5e2d0f9c0811118778c97259ac1ef450570b890d
tac0turtle <markobaricevic@MacBookPro.localdomain>
2025-03-05 12:38:01 +0100

    fix it

--- node/helpers_test.go
+++ node/helpers_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/stretchr/testify/require"
 
-	"github.com/rollkit/rollkit/config"
+	rollkitconfig "github.com/rollkit/rollkit/config"
 	coreda "github.com/rollkit/rollkit/core/da"
 	coreexecutor "github.com/rollkit/rollkit/core/execution"
 	coresequencer "github.com/rollkit/rollkit/core/sequencer"
@@ -27,19 +27,19 @@ func generateSingleKey() crypto.PrivKey {
 	return key
 }
 
-func getTestConfig(n int) config.NodeConfig {
+func getTestConfig(n int) rollkitconfig.NodeConfig {
 	startPort := 10000
-	return config.NodeConfig{
+	return rollkitconfig.NodeConfig{
 		Aggregator:       true,
 		DAAddress:        MockDAAddress,
 		DANamespace:      MockDANamespace,
 		ExecutorAddress:  MockExecutorAddress,
 		SequencerAddress: MockSequencerAddress,
-		BlockManagerConfig: config.BlockManagerConfig{
+		BlockManagerConfig: rollkitconfig.BlockManagerConfig{
 			BlockTime:     500 * time.Millisecond,
 			LazyBlockTime: 5 * time.Second,
 		},
-		P2P: config.P2PConfig{
+		P2P: rollkitconfig.P2PConfig{
 			ListenAddress: "/ip4/127.0.0.1/tcp/" + strconv.Itoa(startPort+n),
 		},
 	}
@@ -70,7 +70,7 @@ func setupTestNodeWithCleanup(t *testing.T) (*FullNode, func()) {
 		p2pKey,
 		signingKey,
 		genesis,
-		DefaultMetricsProvider(config.DefaultInstrumentationConfig()),
+		DefaultMetricsProvider(rollkitconfig.DefaultInstrumentationConfig()),
 		log.NewTestLogger(t),
 	)
 	require.NoError(t, err)
