900cc0ee40202a79138053d6fcb66cb932c4e594
tac0turtle <markobaricevic@Markos-MacBook-Pro.local>
2025-03-13 16:03:26 +0100

    fix dummy client

--- core/da/dummy_client.go
+++ core/da/dummy_client.go
@@ -93,9 +93,7 @@ func (c *DummyClient) RetrieveHeaders(ctx context.Context, dataLayerHeight uint6
 
 	// Convert blobs to headers
 	headers := make([][]byte, len(blobs))
-	for i, blob := range blobs {
-		headers[i] = blob
-	}
+	copy(headers, blobs)
 
 	// Return success result
 	return ResultRetrieveHeaders{
@@ -105,5 +103,80 @@ func (c *DummyClient) RetrieveHeaders(ctx context.Context, dataLayerHeight uint6
 			DAHeight: dataLayerHeight,
 		},
 		Headers: headers,
+	}
+}
+
+// SubmitBatch submits block data to DA layer
+func (c *DummyClient) SubmitBatch(ctx context.Context, data [][]byte, maxBlobSize uint64, gasPrice float64) ResultSubmitBatch {
+	// Convert data to blobs
+	blobs := make([]Blob, len(data))
+	copy(blobs, data)
+
+	// Submit blobs to DA layer
+	_, height, err := c.da.Submit(ctx, blobs, gasPrice, c.namespace, nil)
+	if err != nil {
+		return ResultSubmitBatch{
+			BaseResult: BaseResult{
+				Code:    StatusError,
+				Message: fmt.Sprintf("failed to submit batch data: %v", err),
+			},
+		}
+	}
+
+	// Return success result
+	return ResultSubmitBatch{
+		BaseResult: BaseResult{
+			Code:           StatusSuccess,
+			Message:        fmt.Sprintf("successfully submitted %d items in batch", len(data)),
+			SubmittedCount: uint64(len(data)),
+			DAHeight:       height,
+		},
+	}
+}
+
+// RetrieveBatch retrieves block data from DA layer
+func (c *DummyClient) RetrieveBatch(ctx context.Context, dataLayerHeight uint64) ResultRetrieveBatch {
+	// Get IDs of blobs at the given height
+	result, err := c.da.GetIDs(ctx, dataLayerHeight, c.namespace)
+	if err != nil {
+		return ResultRetrieveBatch{
+			BaseResult: BaseResult{
+				Code:    StatusError,
+				Message: fmt.Sprintf("failed to get IDs at height %d: %v", dataLayerHeight, err),
+			},
+		}
+	}
+
+	// If no blobs at the given height, return empty result
+	if len(result.IDs) == 0 {
+		return ResultRetrieveBatch{
+			BaseResult: BaseResult{
+				Code:     StatusSuccess,
+				Message:  fmt.Sprintf("no data found at height %d", dataLayerHeight),
+				DAHeight: dataLayerHeight,
+			},
+			Data: [][]byte{},
+		}
+	}
+
+	// Get blobs for the IDs
+	blobs, err := c.da.Get(ctx, result.IDs, c.namespace)
+	if err != nil {
+		return ResultRetrieveBatch{
+			BaseResult: BaseResult{
+				Code:    StatusError,
+				Message: fmt.Sprintf("failed to get blobs at height %d: %v", dataLayerHeight, err),
+			},
+		}
+	}
+
+	// Return success result
+	return ResultRetrieveBatch{
+		BaseResult: BaseResult{
+			Code:     StatusSuccess,
+			Message:  fmt.Sprintf("retrieved %d items from batch at height %d", len(blobs), dataLayerHeight),
+			DAHeight: dataLayerHeight,
+		},
+		Data: blobs,
 	}
 }
