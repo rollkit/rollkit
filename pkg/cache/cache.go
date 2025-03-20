8a434aa521342a2bf9d8962256ce664ca80bba4e
tac0turtle <markobaricevic@MacBookPro.localdomain>
2025-03-20 21:05:44 +0100

    linting

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -20,6 +20,7 @@ func NewCache[T any]() *Cache[T] {
 	}
 }
 
+// GetItem returns an item from the cache by height
 func (c *Cache[T]) GetItem(height uint64) *T {
 	item, ok := c.items.Load(height)
 	if !ok {
@@ -29,14 +30,17 @@ func (c *Cache[T]) GetItem(height uint64) *T {
 	return val
 }
 
+// SetItem sets an item in the cache by height
 func (c *Cache[T]) SetItem(height uint64, item *T) {
 	c.items.Store(height, item)
 }
 
+// DeleteItem deletes an item from the cache by height
 func (c *Cache[T]) DeleteItem(height uint64) {
 	c.items.Delete(height)
 }
 
+// IsSeen returns true if the hash has been seen
 func (c *Cache[T]) IsSeen(hash string) bool {
 	seen, ok := c.hashes.Load(hash)
 	if !ok {
@@ -45,10 +49,12 @@ func (c *Cache[T]) IsSeen(hash string) bool {
 	return seen.(bool)
 }
 
+// SetSeen sets the hash as seen
 func (c *Cache[T]) SetSeen(hash string) {
 	c.hashes.Store(hash, true)
 }
 
+// IsDAIncluded returns true if the hash has been DA-included
 func (c *Cache[T]) IsDAIncluded(hash string) bool {
 	daIncluded, ok := c.daIncluded.Load(hash)
 	if !ok {
@@ -57,6 +63,7 @@ func (c *Cache[T]) IsDAIncluded(hash string) bool {
 	return daIncluded.(bool)
 }
 
+// SetDAIncluded sets the hash as DA-included
 func (c *Cache[T]) SetDAIncluded(hash string) {
 	c.daIncluded.Store(hash, true)
 }
