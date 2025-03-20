387b4bbf26f249a8848219999460fed02b29bd06
tac0turtle <markobaricevic@Markos-MacBook-Pro.local>
2025-03-20 11:40:50 +0100

    remove isr

--- types/tx.go
+++ types/tx.go
@@ -1,16 +1,12 @@
 package types
 
 import (
-	"fmt"
-
 	"github.com/cometbft/cometbft/crypto/merkle"
 	"github.com/cometbft/cometbft/crypto/tmhash"
-	cmbytes "github.com/cometbft/cometbft/libs/bytes"
 
 	"github.com/rollkit/rollkit/third_party/celestia-app/appconsts"
 	appns "github.com/rollkit/rollkit/third_party/celestia-app/namespace"
 	"github.com/rollkit/rollkit/third_party/celestia-app/shares"
-	pb "github.com/rollkit/rollkit/types/pb/rollkit/v1"
 )
 
 // Tx represents transaction.
@@ -53,42 +49,9 @@ func (txs Txs) Proof(i int) TxProof {
 
 // TxProof represents a Merkle proof of the presence of a transaction in the Merkle tree.
 type TxProof struct {
-	RootHash cmbytes.HexBytes `json:"root_hash"`
-	Data     Tx               `json:"data"`
-	Proof    merkle.Proof     `json:"proof"`
-}
-
-// ToTxsWithISRs converts a slice of transactions and a list of intermediate state roots
-// to a slice of TxWithISRs. Note that the length of intermediateStateRoots is
-// equal to the length of txs + 1.
-func (txs Txs) ToTxsWithISRs(intermediateStateRoots IntermediateStateRoots) ([]pb.TxWithISRs, error) {
-	expectedISRListLength := len(txs) + 1
-	if len(intermediateStateRoots.RawRootsList) != expectedISRListLength {
-		return nil, fmt.Errorf("invalid length of ISR list: %d, expected length: %d", len(intermediateStateRoots.RawRootsList), expectedISRListLength)
-	}
-	txsWithISRs := make([]pb.TxWithISRs, len(txs))
-	for i, tx := range txs {
-		txsWithISRs[i] = pb.TxWithISRs{
-			PreIsr:  intermediateStateRoots.RawRootsList[i],
-			Tx:      tx,
-			PostIsr: intermediateStateRoots.RawRootsList[i+1],
-		}
-	}
-	return txsWithISRs, nil
-}
-
-// TxsWithISRsToShares converts a slice of TxWithISRs to a slice of shares.
-func TxsWithISRsToShares(txsWithISRs []pb.TxWithISRs) (txShares []shares.Share, err error) {
-	byteSlices := make([][]byte, len(txsWithISRs))
-	for i, txWithISR := range txsWithISRs {
-		byteSlices[i], err = txWithISR.Marshal()
-		if err != nil {
-			return nil, err
-		}
-	}
-	coreTxs := shares.TxsFromBytes(byteSlices)
-	txShares, err = shares.SplitTxs(coreTxs)
-	return txShares, err
+	RootHash []byte       `json:"root_hash"`
+	Data     Tx           `json:"data"`
+	Proof    merkle.Proof `json:"proof"`
 }
 
 // SharesToPostableBytes converts a slice of shares to a slice of bytes that can
@@ -114,22 +77,4 @@ func PostableBytesToShares(postableData []byte) (txShares []shares.Share, err er
 	}
 	shares, _, err := css.Export(0)
 	return shares, err
-}
-
-// SharesToTxsWithISRs converts a slice of shares to a slice of TxWithISRs.
-func SharesToTxsWithISRs(txShares []shares.Share) (txsWithISRs []pb.TxWithISRs, err error) {
-	byteSlices, err := shares.ParseCompactShares(txShares)
-	if err != nil {
-		return nil, err
-	}
-	txsWithISRs = make([]pb.TxWithISRs, len(byteSlices))
-	for i, byteSlice := range byteSlices {
-		var txWithISR pb.TxWithISRs
-		err = txWithISR.Unmarshal(byteSlice)
-		if err != nil {
-			return nil, err
-		}
-		txsWithISRs[i] = txWithISR
-	}
-	return txsWithISRs, nil
 }
