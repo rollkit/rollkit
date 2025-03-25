62853771149095355e922ffc4d366031f197d840
Marko <marko@baricevic.me>
2025-03-25 14:24:26 +0100

    chore: remove mocks and p2p comet deps (#2119)
    This pr removes unused mocks and comet deps from p2p
    
    ---------
    
    Co-authored-by: tac0turtle <markobaricevic@Markos-MacBook-Pro.local>

--- pkg/p2p/peer.go
+++ pkg/p2p/peer.go
@@ -1,11 +1,18 @@
 package p2p
 
-import "github.com/cometbft/cometbft/p2p"
-
 // PeerConnection describe basic information about P2P connection.
 type PeerConnection struct {
-	NodeInfo         p2p.DefaultNodeInfo  `json:"node_info"`
-	IsOutbound       bool                 `json:"is_outbound"`
-	ConnectionStatus p2p.ConnectionStatus `json:"connection_status"`
-	RemoteIP         string               `json:"remote_ip"`
+	NodeInfo   DefaultNodeInfo `json:"node_info"`
+	IsOutbound bool            `json:"is_outbound"`
+	RemoteIP   string          `json:"remote_ip"`
+}
+
+// DefaultNodeInfo is the basic node information exchanged
+// between two peers
+type DefaultNodeInfo struct {
+	DefaultNodeID string `json:"id"`          // authenticated identifier
+	ListenAddr    string `json:"listen_addr"` // accepting incoming
+
+	// Check compatibility.
+	Network string `json:"network"` // network/chain ID
 }
