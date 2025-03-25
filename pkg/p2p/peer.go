78cb67204abacdc9546234821bd1b92e4f5015d3
tac0turtle <markobaricevic@Markos-MacBook-Pro.local>
2025-03-25 12:25:41 +0100

    remove some comet deps

--- pkg/p2p/peer.go
+++ pkg/p2p/peer.go
@@ -1,11 +1,25 @@
 package p2p
 
-import "github.com/cometbft/cometbft/p2p"
-
 // PeerConnection describe basic information about P2P connection.
 type PeerConnection struct {
-	NodeInfo         p2p.DefaultNodeInfo  `json:"node_info"`
-	IsOutbound       bool                 `json:"is_outbound"`
-	ConnectionStatus p2p.ConnectionStatus `json:"connection_status"`
-	RemoteIP         string               `json:"remote_ip"`
+	NodeInfo   DefaultNodeInfo `json:"node_info"`
+	IsOutbound bool            `json:"is_outbound"`
+	RemoteIP   string          `json:"remote_ip"`
+}
+
+// ProtocolVersion contains the protocol versions for the software.
+type ProtocolVersion struct {
+	P2P   uint64 `json:"p2p"`
+	Block uint64 `json:"block"`
+	App   uint64 `json:"app"`
+}
+
+// DefaultNodeInfo is the basic node information exchanged
+// between two peers
+type DefaultNodeInfo struct {
+	DefaultNodeID string `json:"id"`          // authenticated identifier
+	ListenAddr    string `json:"listen_addr"` // accepting incoming
+
+	// Check compatibility.
+	Network string `json:"network"` // network/chain ID
 }
