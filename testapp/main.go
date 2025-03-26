f4735f2f0efd6b03024fc77d11986ee8bfb17786
Randy Grok <randygrok@gmail.com>
2025-03-26 11:11:12 +0100

    remove the intercept commands

--- testapp/main.go
+++ testapp/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	rollcmd "github.com/rollkit/rollkit/pkg/cmd"
-	rollconf "github.com/rollkit/rollkit/pkg/config"
 	testExecutor "github.com/rollkit/rollkit/test/executors/kv"
 	commands "github.com/rollkit/rollkit/testapp/commands"
 )
@@ -24,29 +23,7 @@ func main() {
 		rollcmd.NewRunNodeCmd(testExecutor.CreateDirectKVExecutor(ctx)),
 		rollcmd.VersionCmd,
 		rollcmd.InitCmd,
-		rollcmd.RebuildCmd,
 	)
-
-	// Wrapper function for ReadYaml that doesn't take arguments
-	readYamlWrapper := func() (rollconf.Config, error) {
-		return rollconf.ReadYaml("")
-	}
-
-	// In case there is a rollkit.yaml file in the current dir or somewhere up the
-	// directory tree - we want to intercept the command and execute it against an entrypoint
-	// specified in the rollkit.yaml file. In case of missing yaml file or missing entrypoint key
-	// or missing actual entrypoint file - the normal rootCmd command is executed.
-	executed, err := rollcmd.InterceptCommand(
-		rootCmd,
-		readYamlWrapper,
-		rollcmd.RunRollupEntrypoint,
-	)
-	if err != nil {
-		fmt.Println("Error intercepting command: ", err)
-	}
-	if executed {
-		return
-	}
 
 	if err := rootCmd.Execute(); err != nil {
 		// Print to stderr and exit with error
