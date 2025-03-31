4dd9b7a86cf44302821f5e2c4e2b00aee6d5fc3c
tac0turtle <you@example.com>
2025-03-31 21:45:05 +0200

    fix test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -97,7 +97,7 @@ func TestAddFlags(t *testing.T) {
 	assertFlagValue(t, persistentFlags, FlagLogTrace, false)
 
 	// Count the number of flags we're explicitly checking
-	expectedFlagCount := 44 // Update this number if you add more flag checks above
+	expectedFlagCount := 45 // Update this number if you add more flag checks above
 
 	// Get the actual number of flags (both regular and persistent)
 	actualFlagCount := 0
