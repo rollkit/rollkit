3944be928dc21a9324bf39dbd08bc8abca8ae29c
Julien Robert <julien@rbrt.fr>
2025-04-11 11:08:01 +0200

    updates

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -99,7 +99,7 @@ func TestAddFlags(t *testing.T) {
 	assertFlagValue(t, flags, FlagRPCAddress, DefaultConfig.RPC.Address)
 
 	// Count the number of flags we're explicitly checking
-	expectedFlagCount := 37 // Update this number if you add more flag checks above
+	expectedFlagCount := 35 // Update this number if you add more flag checks above
 
 	// Get the actual number of flags (both regular and persistent)
 	actualFlagCount := 0
