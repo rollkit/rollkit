dccebc0cab3e0c87b6b337be5f1b7223d08fa3dd
Manav Aggarwal <manavaggarwal1234@gmail.com>
2025-05-06 16:59:12 +0100

    feat: Setup docker for evm-single (#2258)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    
    NOTE: PR titles should follow semantic commits:
    https://www.conventionalcommits.org/en/v1.0.0/
    -->
    
    ## Overview
    
    Adds a docker compose setup for easily spinning up evm-single
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    
    Ex: Closes #<issue number>
    -->
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new Docker Compose configuration for deploying an
    Ethereum rollup stack with integrated services.
    - Added a Dockerfile and entrypoint script for streamlined
    containerization and initialization of the EVM single-node rollup
    component.
      - Added a genesis configuration file for the custom EVM chain setup.
    - Included a JWT token file for authentication within the EVM rollup
    environment.
    
    - **Chores**
    - Simplified and cleaned up the Docker build process for improved
    maintainability.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ rollups/evm/single/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+cd /usr/bin
+
+sleep 5
+
+./evm-single init --rollkit.node.aggregator=true --rollkit.signer.passphrase $EVM_SIGNER_PASSPHRASE
+
+# Conditionally add --rollkit.da.address if ROLLKIT_DA_ADDRESS is set
+da_flag=""
+if [ -n "$ROLLKIT_DA_ADDRESS" ]; then
+  da_flag="--rollkit.da.address $ROLLKIT_DA_ADDRESS"
+fi
+
+# Conditionally add --rollkit.da.auth_token if ROLLKIT_DA_AUTH_TOKEN is set
+da_auth_token_flag=""
+if [ -n "$DA_AUTH_TOKEN" ]; then
+  da_auth_token_flag="--rollkit.da.auth_token $ROLLKIT_DA_AUTH_TOKEN"
+fi
+
+# Conditionally add --rollkit.da.namespace if ROLLKIT_DA_NAMESPACE is set
+da_namespace_flag=""
+if [ -n "$DA_NAMESPACE" ]; then
+  da_namespace_flag="--rollkit.da.namespace $ROLLKIT_DA_NAMESPACE"
+fi
+
+exec ./evm-single start \
+  --evm.jwt-secret $EVM_JWT_SECRET \
+  --evm.genesis-hash $EVM_GENESIS_HASH \
+  --evm.engine-url $EVM_ENGINE_URL \
+  --evm.eth-url $EVM_ETH_URL \
+  --rollkit.node.block_time $EVM_BLOCK_TIME \
+  --rollkit.node.aggregator=true \
+  --rollkit.signer.passphrase $EVM_SIGNER_PASSPHRASE \
+  $da_flag \
+  $da_auth_token_flag \
+  $da_namespace_flag 
