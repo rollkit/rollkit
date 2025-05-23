4c50a93ed5ef6d94b8b84f79300774fe1f82d687
Manav Aggarwal <manavaggarwal1234@gmail.com>
2025-05-23 13:28:19 +0200

    Add initial tests

--- node/helpers_test.go
+++ node/helpers_test.go
@@ -7,16 +7,59 @@ import (
 	"time"
 
 	"cosmossdk.io/log"
+	"github.com/ipfs/go-datastore"
+	dssync "github.com/ipfs/go-datastore/sync"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/stretchr/testify/require"
 
+	coreda "github.com/rollkit/rollkit/core/da"
+	coreexecutor "github.com/rollkit/rollkit/core/execution"
+	coresequencer "github.com/rollkit/rollkit/core/sequencer"
+
 	rollkitconfig "github.com/rollkit/rollkit/pkg/config"
+	"github.com/rollkit/rollkit/pkg/p2p"
+	"github.com/rollkit/rollkit/pkg/p2p/key"
 	remote_signer "github.com/rollkit/rollkit/pkg/signer/noop"
 	"github.com/rollkit/rollkit/types"
 )
 
+const (
+	// MockDAAddress is the address used by the mock gRPC service
+	// NOTE: this should be unique per test package to avoid
+	// "bind: listen address already in use" because multiple packages
+	// are tested in parallel
+	MockDAAddress = "grpc://localhost:7990"
+
+	// MockDANamespace is a sample namespace used by the mock DA client
+	MockDANamespace = "00000000000000000000000000000000000000000000000000deadbeef"
+
+	// MockExecutorAddress is a sample address used by the mock executor
+	MockExecutorAddress = "127.0.0.1:40041"
+)
+
+// createTestComponents creates test components for node initialization
+func createTestComponents(t *testing.T, config rollkitconfig.Config) (coreexecutor.Executor, coresequencer.Sequencer, coreda.DA, *p2p.Client, datastore.Batching, *key.NodeKey) {
+	executor := coreexecutor.NewDummyExecutor()
+	sequencer := coresequencer.NewDummySequencer()
+	dummyDA := coreda.NewDummyDA(100_000, 0, 0)
+
+	// Create genesis and keys for P2P client
+	_, genesisValidatorKey, _ := types.GetGenesisWithPrivkey("test-chain")
+	p2pKey := &key.NodeKey{
+		PrivKey: genesisValidatorKey,
+		PubKey:  genesisValidatorKey.GetPublic(),
+	}
+	p2pClient, err := p2p.NewClient(config, p2pKey, dssync.MutexWrap(datastore.NewMapDatastore()), log.NewNopLogger(), p2p.NopMetrics())
+	require.NoError(t, err)
+	require.NotNil(t, p2pClient)
+	ds := dssync.MutexWrap(datastore.NewMapDatastore())
+
+	return executor, sequencer, dummyDA, p2pClient, ds, p2pKey
+}
+
 func getTestConfig(t *testing.T, n int) rollkitconfig.Config {
 	// Use a higher base port to reduce chances of conflicts with system services
-	startPort := 40000 + n*100 // Spread port ranges further apart
+	startPort := 40000 // Spread port ranges further apart
 	return rollkitconfig.Config{
 		RootDir: t.TempDir(),
 		Node: rollkitconfig.NodeConfig{
@@ -33,10 +76,13 @@ func getTestConfig(t *testing.T, n int) rollkitconfig.Config {
 		P2P: rollkitconfig.P2PConfig{
 			ListenAddress: fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", startPort+n),
 		},
+		RPC: rollkitconfig.RPCConfig{
+			Address: fmt.Sprintf("127.0.0.1:%d", 8000+n),
+		},
 	}
 }
 
-func setupTestNodeWithCleanup(t *testing.T, config rollkitconfig.Config) (*FullNode, func()) {
+func createNodeWithCleanup(t *testing.T, config rollkitconfig.Config) (*FullNode, func()) {
 	// Create a cancellable context instead of using background context
 	ctx, cancel := context.WithCancel(context.Background())
 
@@ -45,7 +91,7 @@ func setupTestNodeWithCleanup(t *testing.T, config rollkitconfig.Config) (*FullN
 	remoteSigner, err := remote_signer.NewNoopSigner(genesisValidatorKey)
 	require.NoError(t, err)
 
-	executor, sequencer, dac, p2pClient, ds := createTestComponents(t)
+	executor, sequencer, dac, p2pClient, ds, _ := createTestComponents(t, config)
 
 	require.NoError(t, err)
 
@@ -71,4 +117,78 @@ func setupTestNodeWithCleanup(t *testing.T, config rollkitconfig.Config) (*FullN
 	}
 
 	return node.(*FullNode), cleanup
+}
+
+// Creates the given number of nodes the given nodes using the given wait group to synchronize them
+func createNodesWithCleanup(t *testing.T, num int, config rollkitconfig.Config) ([]*FullNode, []func()) {
+	t.Helper()
+	require := require.New(t)
+
+	nodes := make([]*FullNode, num)
+	cleanups := make([]func(), num)
+	// Create a cancellable context instead of using background context
+	aggCtx, aggCancel := context.WithCancel(context.Background())
+
+	// Generate genesis and keys
+	genesis, genesisValidatorKey, _ := types.GetGenesisWithPrivkey("test-chain")
+	remoteSigner, err := remote_signer.NewNoopSigner(genesisValidatorKey)
+	require.NoError(err)
+
+	aggListenAddress := config.P2P.ListenAddress
+	executor, sequencer, dac, p2pClient, ds, aggP2PKey := createTestComponents(t, config)
+	aggPeerID, err := peer.IDFromPrivateKey(aggP2PKey.PrivKey)
+	require.NoError(err)
+
+	aggNode, err := NewNode(
+		aggCtx,
+		config,
+		executor,
+		sequencer,
+		dac,
+		remoteSigner,
+		p2pClient,
+		genesis,
+		ds,
+		DefaultMetricsProvider(rollkitconfig.DefaultInstrumentationConfig()),
+		log.NewTestLogger(t),
+	)
+	require.NoError(err)
+
+	// Update cleanup to cancel the context instead of calling Stop
+	cleanup := func() {
+		// Cancel the context to stop the node
+		aggCancel()
+	}
+
+	nodes[0], cleanups[0] = aggNode.(*FullNode), cleanup
+	config.Node.Aggregator = false
+	for i := 1; i < num; i++ {
+		ctx, cancel := context.WithCancel(context.Background())
+		config.P2P.ListenAddress = fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", 40001+i)
+		config.RPC.Address = fmt.Sprintf("127.0.0.1:%d", 8000+i+1)
+		config.P2P.Peers = fmt.Sprintf("%s/p2p/%s", aggListenAddress, aggPeerID.Loggable()["peerID"].(string))
+		executor, sequencer, _, p2pClient, ds, _ = createTestComponents(t, config)
+		node, err := NewNode(
+			ctx,
+			config,
+			executor,
+			sequencer,
+			dac,
+			nil,
+			p2pClient,
+			genesis,
+			dssync.MutexWrap(datastore.NewMapDatastore()),
+			DefaultMetricsProvider(rollkitconfig.DefaultInstrumentationConfig()),
+			log.NewTestLogger(t),
+		)
+		require.NoError(err)
+		// Update cleanup to cancel the context instead of calling Stop
+		cleanup := func() {
+			// Cancel the context to stop the node
+			cancel()
+		}
+		nodes[i], cleanups[i] = node.(*FullNode), cleanup
+	}
+
+	return nodes, cleanups
 }
