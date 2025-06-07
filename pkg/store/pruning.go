fe40d10e748f353a2239fad11c3776ca5f39a31a
Eoous <38656355+Eoous@users.noreply.github.com>
2025-06-07 19:14:29 +0000

    add mutex for latestBlockDataHeight

--- pkg/store/pruning.go
+++ pkg/store/pruning.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"sync"
 
 	ds "github.com/ipfs/go-datastore"
 
@@ -15,6 +16,7 @@ type DefaultPruningStore struct {
 
 	config                config.PruningConfig
 	latestBlockDataHeight uint64
+	mu                    sync.Mutex
 }
 
 var _ PruningStore = &DefaultPruningStore{}
@@ -29,9 +31,11 @@ func NewDefaultPruningStore(ds ds.Batching, config config.PruningConfig) Pruning
 
 // SaveBlockData saves the block data and updates the latest block data height.
 func (s *DefaultPruningStore) SaveBlockData(ctx context.Context, header *types.SignedHeader, data *types.Data, signature *types.Signature) error {
-	err := s.SaveBlockData(ctx, header, data, signature)
+	err := s.Store.SaveBlockData(ctx, header, data, signature)
 	if err == nil {
+		s.mu.Lock()
 		s.latestBlockDataHeight = header.Height()
+		s.mu.Unlock()
 	}
 
 	return err
@@ -44,7 +48,9 @@ func (s *DefaultPruningStore) PruneBlockData(ctx context.Context) error {
 	}
 
 	// Read latest height after calling SaveBlockData. There is a delay between SetHeight and SaveBlockData.
+	s.mu.Lock()
 	height := s.latestBlockDataHeight
+	s.mu.Unlock()
 
 	// Skip if not the correct interval or latest height is less or equal than number of blocks need to keep.
 	if height%s.config.Interval != 0 || height < s.config.KeepRecent {
@@ -52,7 +58,7 @@ func (s *DefaultPruningStore) PruneBlockData(ctx context.Context) error {
 	}
 
 	// Must keep at least 2 blocks(while strategy is everything).
-	endHeight := height + 1 - s.config.KeepRecent
+	endHeight := height - 1 - s.config.KeepRecent
 	startHeight := uint64(0)
 	if endHeight > s.config.Interval {
 		startHeight = endHeight - s.config.Interval
