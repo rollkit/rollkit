a12f24b6e988fa96abf0c99eb1b94ab0528579be
Damian Nolan <damiannolan@gmail.com>
2025-07-01 15:09:46 +0200

    feat: add rollkit rust types lib crate

---
+++ crates/rollkit-types/build.rs
@@ -0,0 +1,25 @@
+use std::path::PathBuf;
+use walkdir::WalkDir;
+
+// Collect all .proto files recursively under /proto/ and compile.
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_files: Vec<PathBuf> = WalkDir::new("../../proto")
+        .into_iter()
+        .filter_map(|entry| {
+            let path = entry.ok()?.path().to_path_buf();
+            if path.extension().map_or(false, |ext| ext == "proto") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir("src/proto")
+        .compile(&proto_files, &["../../proto"])?;
+
+    Ok(())
+}
---
+++ crates/rollkit-types/src/proto/mod.rs
@@ -0,0 +1,5 @@
+pub mod rollkit {
+    pub mod v1 {
+        include!("rollkit.v1.rs");
+    }
+}
